50105ae28883f21761887beed180ce9ad0755016
Dan Williams <dcbw@redhat.com>
2009-07-07 10:45:44 -0400

    build: make distcheck actually work

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 REQUIRED_AUTOMAKE_VERSION=1.7
 PKG_NAME=ModemManager
 
-(test -f $srcdir/configure.in \
+(test -f $srcdir/configure.ac \
   && test -f $srcdir/src/mm-modem.c) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
