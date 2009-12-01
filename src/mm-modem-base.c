d269a9a278687c82e5a6ec3f1bba0f91161a05c7
Dan Williams <dcbw@redhat.com>
2009-12-01 12:58:57 -0800

    core: state should always be reset to DISABLED when validity changes
    When the modem becomes valid, it should initially be in disabled state,
    and when it becomes invalid, it should also go to disabled.

--- src/mm-modem-base.c
+++ src/mm-modem-base.c
@@ -150,12 +150,12 @@ mm_modem_base_set_valid (MMModemBase *self, gboolean new_valid)
         priv->valid = new_valid;
         g_object_notify (G_OBJECT (self), MM_MODEM_VALID);
 
-        if (!new_valid) {
-            /* If no longer valid, modem must be disabled */
-            mm_modem_set_state (MM_MODEM (self),
-                                MM_MODEM_STATE_DISABLED,
-                                MM_MODEM_STATE_REASON_NONE);
-        }
+        /* Modem starts off in disabled state, and jumps to disabled when
+         * it's no longer valid.
+         */
+        mm_modem_set_state (MM_MODEM (self),
+                            MM_MODEM_STATE_DISABLED,
+                            MM_MODEM_STATE_REASON_NONE);
     }
 }
 
