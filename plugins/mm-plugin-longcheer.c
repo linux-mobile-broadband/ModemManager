471d8a70a931bd4571ca025dbe0481510d619f2c
Dan Williams <dcbw@redhat.com>
2010-01-15 01:37:26 -0800

    longcheer: new plugin for Longcheer (Alcatel etc) devices (bgo #606550)
    Anything with vendor ID 0x1c9e really; like Alcatel X020, X030,
    X060s, etc.  Longcheer appears to make the actual hardware that all
    the devices with vendor ID 0x1c9e use.  You'll see it in .INF files
    with "CMLONG" as part of the USB interface definition.
    
    If the ports are not correctly detected, we need to get the driver's
    .INF files to determine what the ports should be, and add them to
    the udev rules file.

---
+++ plugins/mm-plugin-longcheer.c
@@ -0,0 +1,180 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2008 - 2009 Novell, Inc.
+ * Copyright (C) 2009 Red Hat, Inc.
+ */
+
+#include <string.h>
+#include <gmodule.h>
+#include "mm-plugin-longcheer.h"
+#include "mm-generic-gsm.h"
+#include "mm-generic-cdma.h"
+
+G_DEFINE_TYPE (MMPluginLongcheer, mm_plugin_longcheer, MM_TYPE_PLUGIN_BASE)
+
+int mm_plugin_major_version = MM_PLUGIN_MAJOR_VERSION;
+int mm_plugin_minor_version = MM_PLUGIN_MINOR_VERSION;
+
+G_MODULE_EXPORT MMPlugin *
+mm_plugin_create (void)
+{
+    return MM_PLUGIN (g_object_new (MM_TYPE_PLUGIN_LONGCHEER,
+                                    MM_PLUGIN_BASE_NAME, "Longcheer",
+                                    NULL));
+}
+
+/*****************************************************************************/
+
+#define CAP_CDMA (MM_PLUGIN_BASE_PORT_CAP_IS707_A | \
+                  MM_PLUGIN_BASE_PORT_CAP_IS707_P | \
+                  MM_PLUGIN_BASE_PORT_CAP_IS856 | \
+                  MM_PLUGIN_BASE_PORT_CAP_IS856_A)
+
+static guint32
+get_level_for_capabilities (guint32 capabilities)
+{
+    if (capabilities & MM_PLUGIN_BASE_PORT_CAP_GSM)
+        return 10;
+    if (capabilities & CAP_CDMA)
+        return 10;
+    return 0;
+}
+
+static void
+probe_result (MMPluginBase *base,
+              MMPluginBaseSupportsTask *task,
+              guint32 capabilities,
+              gpointer user_data)
+{
+    mm_plugin_base_supports_task_complete (task, get_level_for_capabilities (capabilities));
+}
+
+static MMPluginSupportsResult
+supports_port (MMPluginBase *base,
+               MMModem *existing,
+               MMPluginBaseSupportsTask *task)
+{
+    GUdevDevice *port;
+    guint32 cached = 0, level;
+    guint16 vendor = 0;
+    const char *subsys, *name;
+
+    /* Can't do anything with non-serial ports */
+    port = mm_plugin_base_supports_task_get_port (task);
+    if (strcmp (g_udev_device_get_subsystem (port), "tty"))
+        return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+
+    subsys = g_udev_device_get_subsystem (port);
+    name = g_udev_device_get_name (port);
+
+    if (!mm_plugin_base_get_device_ids (base, subsys, name, &vendor, NULL))
+        return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+
+    if (vendor != 0x1c9e)
+        return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+
+    if (mm_plugin_base_get_cached_port_capabilities (base, port, &cached)) {
+        level = get_level_for_capabilities (cached);
+        if (level) {
+            mm_plugin_base_supports_task_complete (task, level);
+            return MM_PLUGIN_SUPPORTS_PORT_IN_PROGRESS;
+        }
+        return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+    }
+
+    /* Otherwise kick off a probe */
+    if (mm_plugin_base_probe_port (base, task, NULL))
+        return MM_PLUGIN_SUPPORTS_PORT_IN_PROGRESS;
+
+    return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+}
+
+static MMModem *
+grab_port (MMPluginBase *base,
+           MMModem *existing,
+           MMPluginBaseSupportsTask *task,
+           GError **error)
+{
+    GUdevDevice *port = NULL, *physdev = NULL;
+    MMModem *modem = NULL;
+    const char *name, *subsys, *sysfs_path;
+    guint32 caps;
+    MMPortType ptype = MM_PORT_TYPE_UNKNOWN;
+
+    port = mm_plugin_base_supports_task_get_port (task);
+    g_assert (port);
+
+    /* Look for port type hints */
+    if (g_udev_device_get_property_as_boolean (port, "ID_MM_LONGCHEER_PORT_TYPE_MODEM"))
+        ptype = MM_PORT_TYPE_PRIMARY;
+    else if (g_udev_device_get_property_as_boolean (port, "ID_MM_LONGCHEER_PORT_TYPE_AUX"))
+        ptype = MM_PORT_TYPE_SECONDARY;
+
+    physdev = mm_plugin_base_supports_task_get_physdev (task);
+    g_assert (physdev);
+    sysfs_path = g_udev_device_get_sysfs_path (physdev);
+    if (!sysfs_path) {
+        g_set_error (error, 0, 0, "Could not get port's physical device sysfs path.");
+        return NULL;
+    }
+
+    subsys = g_udev_device_get_subsystem (port);
+    name = g_udev_device_get_name (port);
+
+    caps = mm_plugin_base_supports_task_get_probed_capabilities (task);
+    if (!existing) {
+        if (caps & MM_PLUGIN_BASE_PORT_CAP_GSM) {
+            modem = mm_generic_gsm_new (sysfs_path,
+                                        mm_plugin_base_supports_task_get_driver (task),
+                                        mm_plugin_get_name (MM_PLUGIN (base)));
+        } else if (caps & CAP_CDMA) {
+            modem = mm_generic_cdma_new (sysfs_path,
+                                         mm_plugin_base_supports_task_get_driver (task),
+                                         mm_plugin_get_name (MM_PLUGIN (base)),
+                                         !!(caps & MM_PLUGIN_BASE_PORT_CAP_IS856),
+                                         !!(caps & MM_PLUGIN_BASE_PORT_CAP_IS856_A));
+        }
+
+        if (modem) {
+            if (!mm_modem_grab_port (modem, subsys, name, ptype, NULL, error)) {
+                g_object_unref (modem);
+                return NULL;
+            }
+        }
+    } else {
+        if (caps & (MM_PLUGIN_BASE_PORT_CAP_GSM | CAP_CDMA)) {
+            modem = existing;
+            if (!mm_modem_grab_port (modem, subsys, name, ptype, NULL, error))
+                return NULL;
+        }
+    }
+
+    return modem;
+}
+
+/*****************************************************************************/
+
+static void
+mm_plugin_longcheer_init (MMPluginLongcheer *self)
+{
+    g_signal_connect (self, "probe-result", G_CALLBACK (probe_result), NULL);
+}
+
+static void
+mm_plugin_longcheer_class_init (MMPluginLongcheerClass *klass)
+{
+    MMPluginBaseClass *pb_class = MM_PLUGIN_BASE_CLASS (klass);
+
+    pb_class->supports_port = supports_port;
+    pb_class->grab_port = grab_port;
+}
