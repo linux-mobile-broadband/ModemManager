79f05cbceca5638dbfef48e5a50bc406ecca5074
Dan Williams <dcbw@redhat.com>
2010-09-22 16:04:25 -0500

    x22x: add simple plugin for Alcatel X220D
    And possibly the X225 as well.  Can't tell much about the modem and
    what commands it supports other than AT+SYSSEL for mode selection.
    The driver software and connection manager for Windows/Mac OS X are
    written by JRD Communication in China, which is a subsidiary of
    TCT, which makes Alcatel-branded phones and data sticks.  But it
    doesn't appear to be the same firmware as other Alcatel/T&A modems
    like X060S and such which are supported by the Longcheer plugin.

---
+++ plugins/mm-plugin-x22x.c
@@ -0,0 +1,184 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2008 - 2009 Novell, Inc.
+ * Copyright (C) 2009 - 2010 Red Hat, Inc.
+ */
+
+#include <string.h>
+#include <gmodule.h>
+#include "mm-plugin-x22x.h"
+#include "mm-modem-x22x-gsm.h"
+#include "mm-generic-gsm.h"
+
+G_DEFINE_TYPE (MMPluginX22x, mm_plugin_x22x, MM_TYPE_PLUGIN_BASE)
+
+int mm_plugin_major_version = MM_PLUGIN_MAJOR_VERSION;
+int mm_plugin_minor_version = MM_PLUGIN_MINOR_VERSION;
+
+G_MODULE_EXPORT MMPlugin *
+mm_plugin_create (void)
+{
+    return MM_PLUGIN (g_object_new (MM_TYPE_PLUGIN_X22X,
+                                    MM_PLUGIN_BASE_NAME, "X22X",
+                                    NULL));
+}
+
+/*****************************************************************************/
+
+static guint32
+get_level_for_capabilities (guint32 capabilities)
+{
+    /* These modems have the same vendor ID as a few of the other
+     * Alcatel/TCT/T&A modems, and the longcheer plugin will try to claim them
+     * too.  So we provide a higher support level here to make sure this
+     * plugin tries to grab it's supported devices first.
+     */
+    if (capabilities & MM_PLUGIN_BASE_PORT_CAP_GSM)
+        return 20;
+    if (capabilities & MM_PLUGIN_BASE_PORT_CAP_QCDM)
+        return 20;
+    return 0;
+}
+
+static void
+probe_result (MMPluginBase *base,
+              MMPluginBaseSupportsTask *task,
+              guint32 capabilities,
+              gpointer user_data)
+{
+    mm_plugin_base_supports_task_complete (task, get_level_for_capabilities (capabilities));
+}
+
+static MMPluginSupportsResult
+supports_port (MMPluginBase *base,
+               MMModem *existing,
+               MMPluginBaseSupportsTask *task)
+{
+    GUdevDevice *port;
+    guint32 cached = 0, level;
+    guint16 vendor = 0;
+    const char *subsys, *name;
+
+    /* Can't do anything with non-serial ports */
+    port = mm_plugin_base_supports_task_get_port (task);
+    if (strcmp (g_udev_device_get_subsystem (port), "tty"))
+        return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+
+    subsys = g_udev_device_get_subsystem (port);
+    name = g_udev_device_get_name (port);
+
+    if (!mm_plugin_base_get_device_ids (base, subsys, name, &vendor, NULL))
+        return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+
+    /* Only TCT/T&A for now */
+    if (vendor != 0x1bbb)
+        return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+
+    /* If the port wasn't tagged, we don't support it */
+    if (!g_udev_device_get_property_as_boolean (port, "ID_MM_X22X_TAGGED"))
+        return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+
+    if (mm_plugin_base_get_cached_port_capabilities (base, port, &cached)) {
+        level = get_level_for_capabilities (cached);
+        if (level) {
+            mm_plugin_base_supports_task_complete (task, level);
+            return MM_PLUGIN_SUPPORTS_PORT_IN_PROGRESS;
+        }
+        return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+    }
+
+    /* Otherwise kick off a probe */
+    if (mm_plugin_base_probe_port (base, task, NULL))
+        return MM_PLUGIN_SUPPORTS_PORT_IN_PROGRESS;
+
+    return MM_PLUGIN_SUPPORTS_PORT_UNSUPPORTED;
+}
+
+static MMModem *
+grab_port (MMPluginBase *base,
+           MMModem *existing,
+           MMPluginBaseSupportsTask *task,
+           GError **error)
+{
+    GUdevDevice *port = NULL;
+    MMModem *modem = NULL;
+    const char *name, *subsys, *sysfs_path;
+    guint32 caps;
+    MMPortType ptype = MM_PORT_TYPE_UNKNOWN;
+
+    port = mm_plugin_base_supports_task_get_port (task);
+    g_assert (port);
+
+    /* Look for port type hints; just probing can't distinguish which port should
+     * be the data/primary port on these devices.  We have to tag them based on
+     * what the Windows .INF files say the port layout should be.
+     */
+    if (g_udev_device_get_property_as_boolean (port, "ID_MM_X22X_PORT_TYPE_MODEM"))
+        ptype = MM_PORT_TYPE_PRIMARY;
+    else if (g_udev_device_get_property_as_boolean (port, "ID_MM_X22X_PORT_TYPE_AUX"))
+        ptype = MM_PORT_TYPE_SECONDARY;
+
+    /* If the device was tagged by the udev rules, then ignore any other ports
+     * to guard against race conditions if a device just happens to show up
+     * with more than two AT-capable ports.
+     */
+    if (   (ptype == MM_PORT_TYPE_UNKNOWN)
+        && g_udev_device_get_property_as_boolean (port, "ID_MM_X22X_TAGGED"))
+        ptype = MM_PORT_TYPE_IGNORED;
+
+    subsys = g_udev_device_get_subsystem (port);
+    name = g_udev_device_get_name (port);
+
+    caps = mm_plugin_base_supports_task_get_probed_capabilities (task);
+    sysfs_path = mm_plugin_base_supports_task_get_physdev_path (task);
+    if (!existing) {
+        if (caps & MM_PLUGIN_BASE_PORT_CAP_GSM) {
+            modem = mm_modem_x22x_gsm_new (sysfs_path,
+                                           mm_plugin_base_supports_task_get_driver (task),
+                                           mm_plugin_get_name (MM_PLUGIN (base)));
+        }
+
+        if (modem) {
+            if (!mm_modem_grab_port (modem, subsys, name, ptype, NULL, error)) {
+                g_object_unref (modem);
+                return NULL;
+            }
+        }
+    } else if (get_level_for_capabilities (caps)) {
+        if (caps & MM_PLUGIN_BASE_PORT_CAP_QCDM)
+            ptype = MM_PORT_TYPE_QCDM;
+
+        modem = existing;
+        if (!mm_modem_grab_port (modem, subsys, name, ptype, NULL, error))
+            return NULL;
+    }
+
+    return modem;
+}
+
+/*****************************************************************************/
+
+static void
+mm_plugin_x22x_init (MMPluginX22x *self)
+{
+    g_signal_connect (self, "probe-result", G_CALLBACK (probe_result), NULL);
+}
+
+static void
+mm_plugin_x22x_class_init (MMPluginX22xClass *klass)
+{
+    MMPluginBaseClass *pb_class = MM_PLUGIN_BASE_CLASS (klass);
+
+    pb_class->supports_port = supports_port;
+    pb_class->grab_port = grab_port;
+}
