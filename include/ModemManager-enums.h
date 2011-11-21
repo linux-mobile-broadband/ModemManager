ff86bc9f4a33d3fbdfce22b4e6d580530278949d
Aleksander Morgado <aleksander@lanedo.com>
2011-11-21 16:31:26 +0100

    api,enums: new enum for the USSD session state

--- include/ModemManager-enums.h
+++ include/ModemManager-enums.h
@@ -468,4 +468,18 @@ typedef enum { /*< underscore_name=mm_modem_3gpp_network_availability >*/
     MM_MODEM_3GPP_NETWORK_AVAILABILITY_FORBIDDEN = 3,
 } MMModem3gppNetworkAvailability;
 
+/**
+ * MMModem3gppUssdSessionState:
+ * @MM_MODEM_3GPP_USSD_SESSION_STATE_IDLE: No active session.
+ * @MM_MODEM_3GPP_USSD_SESSION_STATE_ACTIVE: A session is active and the mobile is waiting for a response.
+ * @MM_MODEM_3GPP_USSD_SESSION_STATE_USER_RESPONSE: The network is waiting for the client's response.
+ *
+ * State of a USSD session.
+ */
+typedef enum { /*< underscore_name=mm_modem_3gpp_ussd_session_state >*/
+    MM_MODEM_3GPP_USSD_SESSION_STATE_IDLE          = 0,
+    MM_MODEM_3GPP_USSD_SESSION_STATE_ACTIVE        = 1,
+    MM_MODEM_3GPP_USSD_SESSION_STATE_USER_RESPONSE = 2,
+} MMModem3gppUssdSessionState;
+
 #endif /*  _MODEMMANAGER_ENUMS_H_ */
