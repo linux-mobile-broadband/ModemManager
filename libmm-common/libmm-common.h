60819197441e191c662f93da2691c4cb1a13385e
Aleksander Morgado <aleksander@lanedo.com>
2011-12-19 18:08:24 +0100

    libmm-common: make it installable
    We'll install libmm-common and its headers, which will be used by libmm-glib
    and the ModemManager daemon.

---
+++ libmm-common/libmm-common.h
@@ -0,0 +1,34 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * libmm-common -- Common library used by libmm-glib and ModemManager
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ * Copyright (C) 2011 Google, Inc.
+ */
+
+#ifndef _LIBMM_COMMON_H_
+#define _LIBMM_COMMON_H_
+
+#include "mm-errors-types.h"
+#include "mm-enums-types.h"
+#include "mm-common-helpers.h"
+#include "mm-gdbus-manager.h"
+#include "mm-gdbus-modem.h"
+#include "mm-gdbus-bearer.h"
+#include "mm-gdbus-sim.h"
+
+#endif /* _LIBMM_COMMON_H_ */
