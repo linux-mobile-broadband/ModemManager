59e903820d21dc041443ebb05348ee305d269429
Aleksander Morgado <aleksander@lanedo.com>
2012-01-22 12:55:00 +0100

    api,header: new MM_ACCESS_TECHNOLOGY_ANY
    Helps as a mask covering all the available access technologies.

--- include/ModemManager-enums.h
+++ include/ModemManager-enums.h
@@ -150,6 +150,7 @@ typedef enum { /*< underscore_name=mm_modem_state_change_reason >*/
  * @MM_MODEM_ACCESS_TECHNOLOGY_EVDOA: CDMA2000 EVDO revision A.
  * @MM_MODEM_ACCESS_TECHNOLOGY_EVDOB: CDMA2000 EVDO revision B.
  * @MM_MODEM_ACCESS_TECHNOLOGY_LTE: LTE (ETSI 27.007: "E-UTRAN")
+ * @MM_MODEM_ACCESS_TECHNOLOGY_ANY: Mask specifying all access technologies.
  *
  * Describes various access technologies that a device uses when registered with
  * or connected to a network.
@@ -171,6 +172,7 @@ typedef enum { /*< underscore_name=mm_modem_access_technology >*/
     MM_MODEM_ACCESS_TECHNOLOGY_EVDOA       = 1 << 12,
     MM_MODEM_ACCESS_TECHNOLOGY_EVDOB       = 1 << 13,
     MM_MODEM_ACCESS_TECHNOLOGY_LTE         = 1 << 14,
+    MM_MODEM_ACCESS_TECHNOLOGY_ANY         = 0xFFFFFFFF,
 } MMModemAccessTechnology;
 
 /**
