bc0454badf7df37f67af2e327da6ea54b5f558e6
Aleksander Morgado <aleksander@lanedo.com>
2012-01-25 12:51:08 +0100

    iface-modem-messaging: new interface to handle the Messaging DBus interface

---
+++ src/mm-iface-modem-messaging.c
@@ -0,0 +1,379 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2012 Google, Inc.
+ */
+
+#include <ModemManager.h>
+#include <libmm-common.h>
+
+#include "mm-iface-modem.h"
+#include "mm-iface-modem-messaging.h"
+#include "mm-log.h"
+
+/*****************************************************************************/
+
+void
+mm_iface_modem_messaging_bind_simple_status (MMIfaceModemMessaging *self,
+                                            MMCommonSimpleProperties *status)
+{
+}
+
+/*****************************************************************************/
+
+typedef struct _DisablingContext DisablingContext;
+static void interface_disabling_step (DisablingContext *ctx);
+
+typedef enum {
+    DISABLING_STEP_FIRST,
+    DISABLING_STEP_LAST
+} DisablingStep;
+
+struct _DisablingContext {
+    MMIfaceModemMessaging *self;
+    MMAtSerialPort *primary;
+    DisablingStep step;
+    GSimpleAsyncResult *result;
+    MmGdbusModemMessaging *skeleton;
+};
+
+static DisablingContext *
+disabling_context_new (MMIfaceModemMessaging *self,
+                       GAsyncReadyCallback callback,
+                       gpointer user_data)
+{
+    DisablingContext *ctx;
+
+    ctx = g_new0 (DisablingContext, 1);
+    ctx->self = g_object_ref (self);
+    ctx->primary = g_object_ref (mm_base_modem_get_port_primary (MM_BASE_MODEM (self)));
+    ctx->result = g_simple_async_result_new (G_OBJECT (self),
+                                             callback,
+                                             user_data,
+                                             disabling_context_new);
+    ctx->step = DISABLING_STEP_FIRST;
+    g_object_get (ctx->self,
+                  MM_IFACE_MODEM_MESSAGING_DBUS_SKELETON, &ctx->skeleton,
+                  NULL);
+    g_assert (ctx->skeleton != NULL);
+
+    return ctx;
+}
+
+static void
+disabling_context_complete_and_free (DisablingContext *ctx)
+{
+    g_simple_async_result_complete_in_idle (ctx->result);
+    g_object_unref (ctx->self);
+    g_object_unref (ctx->primary);
+    g_object_unref (ctx->result);
+    g_object_unref (ctx->skeleton);
+    g_free (ctx);
+}
+
+gboolean
+mm_iface_modem_messaging_disable_finish (MMIfaceModemMessaging *self,
+                                        GAsyncResult *res,
+                                        GError **error)
+{
+    return !g_simple_async_result_propagate_error (G_SIMPLE_ASYNC_RESULT (res), error);
+}
+
+static void
+interface_disabling_step (DisablingContext *ctx)
+{
+    switch (ctx->step) {
+    case DISABLING_STEP_FIRST:
+        /* Fall down to next step */
+        ctx->step++;
+
+    case DISABLING_STEP_LAST:
+        /* We are done without errors! */
+        g_simple_async_result_set_op_res_gboolean (ctx->result, TRUE);
+        disabling_context_complete_and_free (ctx);
+        return;
+    }
+
+    g_assert_not_reached ();
+}
+
+void
+mm_iface_modem_messaging_disable (MMIfaceModemMessaging *self,
+                                 GAsyncReadyCallback callback,
+                                 gpointer user_data)
+{
+    interface_disabling_step (disabling_context_new (self,
+                                                     callback,
+                                                     user_data));
+}
+
+/*****************************************************************************/
+
+typedef struct _EnablingContext EnablingContext;
+static void interface_enabling_step (EnablingContext *ctx);
+
+typedef enum {
+    ENABLING_STEP_FIRST,
+    ENABLING_STEP_LAST
+} EnablingStep;
+
+struct _EnablingContext {
+    MMIfaceModemMessaging *self;
+    MMAtSerialPort *primary;
+    EnablingStep step;
+    GSimpleAsyncResult *result;
+    MmGdbusModemMessaging *skeleton;
+};
+
+static EnablingContext *
+enabling_context_new (MMIfaceModemMessaging *self,
+                      GAsyncReadyCallback callback,
+                      gpointer user_data)
+{
+    EnablingContext *ctx;
+
+    ctx = g_new0 (EnablingContext, 1);
+    ctx->self = g_object_ref (self);
+    ctx->primary = g_object_ref (mm_base_modem_get_port_primary (MM_BASE_MODEM (self)));
+    ctx->result = g_simple_async_result_new (G_OBJECT (self),
+                                             callback,
+                                             user_data,
+                                             enabling_context_new);
+    ctx->step = ENABLING_STEP_FIRST;
+    g_object_get (ctx->self,
+                  MM_IFACE_MODEM_MESSAGING_DBUS_SKELETON, &ctx->skeleton,
+                  NULL);
+    g_assert (ctx->skeleton != NULL);
+
+    return ctx;
+}
+
+static void
+enabling_context_complete_and_free (EnablingContext *ctx)
+{
+    g_simple_async_result_complete_in_idle (ctx->result);
+    g_object_unref (ctx->self);
+    g_object_unref (ctx->primary);
+    g_object_unref (ctx->result);
+    g_object_unref (ctx->skeleton);
+    g_free (ctx);
+}
+
+gboolean
+mm_iface_modem_messaging_enable_finish (MMIfaceModemMessaging *self,
+                                        GAsyncResult *res,
+                                        GError **error)
+{
+    return !g_simple_async_result_propagate_error (G_SIMPLE_ASYNC_RESULT (res), error);
+}
+
+static void
+interface_enabling_step (EnablingContext *ctx)
+{
+    switch (ctx->step) {
+    case ENABLING_STEP_FIRST:
+        /* Fall down to next step */
+        ctx->step++;
+
+    case ENABLING_STEP_LAST:
+        /* We are done without errors! */
+        g_simple_async_result_set_op_res_gboolean (ctx->result, TRUE);
+        enabling_context_complete_and_free (ctx);
+        return;
+    }
+
+    g_assert_not_reached ();
+}
+
+void
+mm_iface_modem_messaging_enable (MMIfaceModemMessaging *self,
+                                GAsyncReadyCallback callback,
+                                gpointer user_data)
+{
+    interface_enabling_step (enabling_context_new (self,
+                                                   callback,
+                                                   user_data));
+}
+
+/*****************************************************************************/
+
+typedef struct _InitializationContext InitializationContext;
+static void interface_initialization_step (InitializationContext *ctx);
+
+typedef enum {
+    INITIALIZATION_STEP_FIRST,
+    INITIALIZATION_STEP_LAST
+} InitializationStep;
+
+struct _InitializationContext {
+    MMIfaceModemMessaging *self;
+    MMAtSerialPort *port;
+    MmGdbusModemMessaging *skeleton;
+    GSimpleAsyncResult *result;
+    InitializationStep step;
+};
+
+static InitializationContext *
+initialization_context_new (MMIfaceModemMessaging *self,
+                            MMAtSerialPort *port,
+                            GAsyncReadyCallback callback,
+                            gpointer user_data)
+{
+    InitializationContext *ctx;
+
+    ctx = g_new0 (InitializationContext, 1);
+    ctx->self = g_object_ref (self);
+    ctx->port = g_object_ref (port);
+    ctx->result = g_simple_async_result_new (G_OBJECT (self),
+                                             callback,
+                                             user_data,
+                                             initialization_context_new);
+    ctx->step = INITIALIZATION_STEP_FIRST;
+    g_object_get (ctx->self,
+                  MM_IFACE_MODEM_MESSAGING_DBUS_SKELETON, &ctx->skeleton,
+                  NULL);
+    g_assert (ctx->skeleton != NULL);
+    return ctx;
+}
+
+static void
+initialization_context_complete_and_free (InitializationContext *ctx)
+{
+    g_simple_async_result_complete_in_idle (ctx->result);
+    g_object_unref (ctx->self);
+    g_object_unref (ctx->port);
+    g_object_unref (ctx->result);
+    g_object_unref (ctx->skeleton);
+    g_free (ctx);
+}
+
+static void
+interface_initialization_step (InitializationContext *ctx)
+{
+    switch (ctx->step) {
+    case INITIALIZATION_STEP_FIRST:
+        /* Fall down to next step */
+        ctx->step++;
+
+    case INITIALIZATION_STEP_LAST:
+        /* We are done without errors! */
+
+        /* Finally, export the new interface */
+        mm_gdbus_object_skeleton_set_modem_messaging (MM_GDBUS_OBJECT_SKELETON (ctx->self),
+                                                      MM_GDBUS_MODEM_MESSAGING (ctx->skeleton));
+
+        g_simple_async_result_set_op_res_gboolean (ctx->result, TRUE);
+        initialization_context_complete_and_free (ctx);
+        return;
+    }
+
+    g_assert_not_reached ();
+}
+
+gboolean
+mm_iface_modem_messaging_initialize_finish (MMIfaceModemMessaging *self,
+                                           GAsyncResult *res,
+                                           GError **error)
+{
+    g_return_val_if_fail (MM_IS_IFACE_MODEM_MESSAGING (self), FALSE);
+    g_return_val_if_fail (G_IS_ASYNC_RESULT (res), FALSE);
+
+    return !g_simple_async_result_propagate_error (G_SIMPLE_ASYNC_RESULT (res), error);
+}
+
+void
+mm_iface_modem_messaging_initialize (MMIfaceModemMessaging *self,
+                                    MMAtSerialPort *port,
+                                    GAsyncReadyCallback callback,
+                                    gpointer user_data)
+{
+    MmGdbusModemMessaging *skeleton = NULL;
+
+    g_return_if_fail (MM_IS_IFACE_MODEM_MESSAGING (self));
+
+    /* Did we already create it? */
+    g_object_get (self,
+                  MM_IFACE_MODEM_MESSAGING_DBUS_SKELETON, &skeleton,
+                  NULL);
+    if (!skeleton) {
+        skeleton = mm_gdbus_modem_messaging_skeleton_new ();
+
+        g_object_set (self,
+                      MM_IFACE_MODEM_MESSAGING_DBUS_SKELETON, skeleton,
+                      NULL);
+    }
+
+
+    /* Perform async initialization here */
+    interface_initialization_step (initialization_context_new (self,
+                                                               port,
+                                                               callback,
+                                                               user_data));
+    g_object_unref (skeleton);
+}
+
+void
+mm_iface_modem_messaging_shutdown (MMIfaceModemMessaging *self)
+{
+    g_return_if_fail (MM_IS_IFACE_MODEM_MESSAGING (self));
+
+    /* Unexport DBus interface and remove the skeleton */
+    mm_gdbus_object_skeleton_set_modem_messaging (MM_GDBUS_OBJECT_SKELETON (self), NULL);
+    g_object_set (self,
+                  MM_IFACE_MODEM_MESSAGING_DBUS_SKELETON, NULL,
+                  NULL);
+}
+
+/*****************************************************************************/
+
+static void
+iface_modem_messaging_init (gpointer g_iface)
+{
+    static gboolean initialized = FALSE;
+
+    if (initialized)
+        return;
+
+    /* Properties */
+    g_object_interface_install_property
+        (g_iface,
+         g_param_spec_object (MM_IFACE_MODEM_MESSAGING_DBUS_SKELETON,
+                              "Messaging DBus skeleton",
+                              "DBus skeleton for the Messaging interface",
+                              MM_GDBUS_TYPE_MODEM_MESSAGING_SKELETON,
+                              G_PARAM_READWRITE));
+
+    initialized = TRUE;
+}
+
+GType
+mm_iface_modem_messaging_get_type (void)
+{
+    static GType iface_modem_messaging_type = 0;
+
+    if (!G_UNLIKELY (iface_modem_messaging_type)) {
+        static const GTypeInfo info = {
+            sizeof (MMIfaceModemMessaging), /* class_size */
+            iface_modem_messaging_init,     /* base_init */
+            NULL,                           /* base_finalize */
+        };
+
+        iface_modem_messaging_type = g_type_register_static (G_TYPE_INTERFACE,
+                                                             "MMIfaceModemMessaging",
+                                                             &info,
+                                                             0);
+
+        g_type_interface_add_prerequisite (iface_modem_messaging_type, MM_TYPE_IFACE_MODEM);
+    }
+
+    return iface_modem_messaging_type;
+}
---
+++ src/mm-iface-modem-messaging.h
@@ -0,0 +1,71 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2012 Google, Inc.
+ */
+
+#ifndef MM_IFACE_MODEM_MESSAGING_H
+#define MM_IFACE_MODEM_MESSAGING_H
+
+#include <glib-object.h>
+#include <gio/gio.h>
+
+#include "mm-at-serial-port.h"
+
+#define MM_TYPE_IFACE_MODEM_MESSAGING               (mm_iface_modem_messaging_get_type ())
+#define MM_IFACE_MODEM_MESSAGING(obj)               (G_TYPE_CHECK_INSTANCE_CAST ((obj), MM_TYPE_IFACE_MODEM_MESSAGING, MMIfaceModemMessaging))
+#define MM_IS_IFACE_MODEM_MESSAGING(obj)            (G_TYPE_CHECK_INSTANCE_TYPE ((obj), MM_TYPE_IFACE_MODEM_MESSAGING))
+#define MM_IFACE_MODEM_MESSAGING_GET_INTERFACE(obj) (G_TYPE_INSTANCE_GET_INTERFACE ((obj), MM_TYPE_IFACE_MODEM_MESSAGING, MMIfaceModemMessaging))
+
+#define MM_IFACE_MODEM_MESSAGING_DBUS_SKELETON "iface-modem-messaging-dbus-skeleton"
+
+typedef struct _MMIfaceModemMessaging MMIfaceModemMessaging;
+
+struct _MMIfaceModemMessaging {
+    GTypeInterface g_iface;
+};
+
+GType mm_iface_modem_messaging_get_type (void);
+
+/* Initialize Messaging interface (async) */
+void     mm_iface_modem_messaging_initialize        (MMIfaceModemMessaging *self,
+                                                     MMAtSerialPort *port,
+                                                     GAsyncReadyCallback callback,
+                                                     gpointer user_data);
+gboolean mm_iface_modem_messaging_initialize_finish (MMIfaceModemMessaging *self,
+                                                     GAsyncResult *res,
+                                                     GError **error);
+
+/* Enable Messaging interface (async) */
+void     mm_iface_modem_messaging_enable        (MMIfaceModemMessaging *self,
+                                                 GAsyncReadyCallback callback,
+                                                 gpointer user_data);
+gboolean mm_iface_modem_messaging_enable_finish (MMIfaceModemMessaging *self,
+                                                 GAsyncResult *res,
+                                                 GError **error);
+
+/* Disable Messaging interface (async) */
+void     mm_iface_modem_messaging_disable        (MMIfaceModemMessaging *self,
+                                                  GAsyncReadyCallback callback,
+                                                  gpointer user_data);
+gboolean mm_iface_modem_messaging_disable_finish (MMIfaceModemMessaging *self,
+                                                  GAsyncResult *res,
+                                                  GError **error);
+
+/* Shutdown Messaging interface */
+void mm_iface_modem_messaging_shutdown (MMIfaceModemMessaging *self);
+
+/* Bind properties for simple GetStatus() */
+void mm_iface_modem_messaging_bind_simple_status (MMIfaceModemMessaging *self,
+                                                  MMCommonSimpleProperties *status);
+
+#endif /* MM_IFACE_MODEM_MESSAGING_H */
