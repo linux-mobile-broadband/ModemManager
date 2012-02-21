7316f4645bf82a8586507a4f7abf57bf9470d309
Aleksander Morgado <aleksander@lanedo.com>
2012-02-21 12:26:29 +0100

    iface-modem-3gpp-ussd: fix copyrights

--- src/mm-iface-modem-3gpp-ussd.h
+++ src/mm-iface-modem-3gpp-ussd.h
@@ -10,6 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details:
  *
+ * Copyright (C) 2010 Guido Guenther <agx@sigxcpu.org>
  * Copyright (C) 2012 Google, Inc.
  */
 
