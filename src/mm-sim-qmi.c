742527aa606c73139fd8046c506362ed8c6ffef5
Aleksander Morgado <aleksander@lanedo.com>
2012-07-02 13:04:48 +0200

    sim-qmi: update SIM ID (ICCID) using QMI

--- src/mm-sim-qmi.c
+++ src/mm-sim-qmi.c
@@ -23,9 +23,119 @@
 #include <ModemManager.h>
 #include <libmm-common.h>
 
+#include "mm-log.h"
 #include "mm-sim-qmi.h"
 
 G_DEFINE_TYPE (MMSimQmi, mm_sim_qmi, MM_TYPE_SIM);
+
+/*****************************************************************************/
+
+static gboolean
+ensure_qmi_client (MMSimQmi *self,
+                   QmiService service,
+                   QmiClient **o_client,
+                   GAsyncReadyCallback callback,
+                   gpointer user_data)
+{
+    MMBaseModem *modem = NULL;
+    QmiClient *client;
+
+    g_object_get (self,
+                  MM_SIM_MODEM, &modem,
+                  NULL);
+    g_assert (MM_IS_BASE_MODEM (modem));
+
+    client = mm_qmi_port_peek_client (mm_base_modem_peek_port_qmi (modem), service);
+    if (!client)
+        g_simple_async_report_error_in_idle (G_OBJECT (self),
+                                             callback,
+                                             user_data,
+                                             MM_CORE_ERROR,
+                                             MM_CORE_ERROR_FAILED,
+                                             "Couldn't peek client for service '%s'",
+                                             qmi_service_get_string (service));
+    else
+        *o_client = client;
+
+    g_object_unref (modem);
+    return !!client;
+}
+
+/*****************************************************************************/
+/* Load SIM ID (ICCID) */
+
+static gchar *
+load_sim_identifier_finish (MMSim *self,
+                            GAsyncResult *res,
+                            GError **error)
+{
+    gchar *sim_identifier;
+
+    if (g_simple_async_result_propagate_error (G_SIMPLE_ASYNC_RESULT (res), error))
+        return NULL;
+
+    sim_identifier = g_strdup (g_simple_async_result_get_op_res_gpointer (G_SIMPLE_ASYNC_RESULT (res)));
+    mm_dbg ("loaded SIM identifier: %s", sim_identifier);
+    return sim_identifier;
+}
+
+static void
+dms_uim_get_iccid_ready (QmiClientDms *client,
+                         GAsyncResult *res,
+                         GSimpleAsyncResult *simple)
+{
+    QmiMessageDmsUimGetIccidOutput *output = NULL;
+    GError *error = NULL;
+
+    output = qmi_client_dms_uim_get_iccid_finish (client, res, &error);
+    if (!output) {
+        g_prefix_error (&error, "QMI operation failed: ");
+        g_simple_async_result_take_error (simple, error);
+    } else if (!qmi_message_dms_uim_get_iccid_output_get_result (output, &error)) {
+        g_prefix_error (&error, "Couldn't get UIM ICCID: ");
+        g_simple_async_result_take_error (simple, error);
+    } else {
+        const gchar *str = NULL;
+
+        qmi_message_dms_uim_get_iccid_output_get_iccid (output, &str, NULL);
+        g_simple_async_result_set_op_res_gpointer (simple,
+                                                   g_strdup (str),
+                                                   (GDestroyNotify)g_free);
+    }
+
+    if (output)
+        qmi_message_dms_uim_get_iccid_output_unref (output);
+
+    g_simple_async_result_complete (simple);
+    g_object_unref (simple);
+}
+
+static void
+load_sim_identifier (MMSim *self,
+                     GAsyncReadyCallback callback,
+                     gpointer user_data)
+{
+    GSimpleAsyncResult *result;
+    QmiClient *client = NULL;
+
+    if (!ensure_qmi_client (MM_SIM_QMI (self),
+                            QMI_SERVICE_DMS, &client,
+                            callback, user_data))
+        return;
+
+    result = g_simple_async_result_new (G_OBJECT (self),
+                                        callback,
+                                        user_data,
+                                        load_sim_identifier);
+
+    mm_dbg ("loading SIM identifier...");
+    qmi_client_dms_uim_get_iccid (QMI_CLIENT_DMS (client),
+                                  NULL,
+                                  5,
+                                  NULL,
+                                  (GAsyncReadyCallback)dms_uim_get_iccid_ready,
+                                  result);
+}
 
 /*****************************************************************************/
 
@@ -72,4 +182,8 @@ mm_sim_qmi_init (MMSimQmi *self)
 static void
 mm_sim_qmi_class_init (MMSimQmiClass *klass)
 {
+    MMSimClass *sim_class = MM_SIM_CLASS (klass);
+
+    sim_class->load_sim_identifier = load_sim_identifier;
+    sim_class->load_sim_identifier_finish = load_sim_identifier_finish;
 }
