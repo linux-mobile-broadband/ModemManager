5c2df4a0cac1675d31a94d1ee5f6b9c113e8ed35
Aleksander Morgado <aleksander@lanedo.com>
2012-07-21 18:17:24 +0200

    nokia-icera: new plugin to handle Icera-based Nokia modems
    We have a whole new plugin because the whole probing process is different
    between the Icera and non-Icera based Nokia modems.

---
+++ plugins/nokia/mm-plugin-nokia-icera.c
@@ -0,0 +1,106 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2009 - 2012 Red Hat, Inc.
+ * Copyright (C) 2012 Aleksander Morgado <aleksander@gnu.org>
+ */
+
+#include <string.h>
+#include <gmodule.h>
+
+#include <libmm-common.h>
+
+#include "mm-log.h"
+#include "mm-plugin-nokia-icera.h"
+#include "mm-broadband-modem-icera.h"
+
+G_DEFINE_TYPE (MMPluginNokiaIcera, mm_plugin_nokia_icera, MM_TYPE_PLUGIN)
+
+int mm_plugin_major_version = MM_PLUGIN_MAJOR_VERSION;
+int mm_plugin_minor_version = MM_PLUGIN_MINOR_VERSION;
+
+/*****************************************************************************/
+
+static MMBaseModem *
+create_modem (MMPlugin *self,
+              const gchar *sysfs_path,
+              const gchar *driver,
+              guint16 vendor,
+              guint16 product,
+              GList *probes,
+              GError **error)
+{
+    return MM_BASE_MODEM (mm_broadband_modem_icera_new (sysfs_path,
+                                                        driver,
+                                                        mm_plugin_get_name (self),
+                                                        vendor,
+                                                        product));
+}
+
+static gboolean
+grab_port (MMPlugin *self,
+           MMBaseModem *modem,
+           MMPortProbe *probe,
+           GError **error)
+{
+    GUdevDevice *port;
+    MMAtPortFlag pflags = MM_AT_PORT_FLAG_NONE;
+
+    port = mm_port_probe_peek_port (probe);
+
+    /* Look for port type hints */
+    if (mm_port_probe_is_at (probe)) {
+        if (g_udev_device_get_property_as_boolean (port, "ID_MM_NOKIA_PORT_TYPE_MODEM"))
+            pflags = MM_AT_PORT_FLAG_PRIMARY;
+        else if (g_udev_device_get_property_as_boolean (port, "ID_MM_NOKIA_PORT_TYPE_AUX"))
+            pflags = MM_AT_PORT_FLAG_SECONDARY;
+    }
+
+    return mm_base_modem_grab_port (modem,
+                                    mm_port_probe_get_port_subsys (probe),
+                                    mm_port_probe_get_port_name (probe),
+                                    mm_port_probe_get_port_type (probe),
+                                    pflags,
+                                    error);
+}
+
+/*****************************************************************************/
+
+G_MODULE_EXPORT MMPlugin *
+mm_plugin_create (void)
+{
+    static const gchar *subsystems[] = { "tty", "net", NULL };
+    static const guint16 vendor_ids[] = { 0x0421, 0 };
+
+    return MM_PLUGIN (
+        g_object_new (MM_TYPE_PLUGIN_NOKIA_ICERA,
+                      MM_PLUGIN_NAME,               "Nokia (Icera)",
+                      MM_PLUGIN_ALLOWED_SUBSYSTEMS, subsystems,
+                      MM_PLUGIN_ALLOWED_VENDOR_IDS, vendor_ids,
+                      MM_PLUGIN_ALLOWED_AT,         TRUE,
+                      MM_PLUGIN_ALLOWED_ICERA,      TRUE, /* Only Nokia/Icera modems */
+                      NULL));
+}
+
+static void
+mm_plugin_nokia_icera_init (MMPluginNokiaIcera *self)
+{
+}
+
+static void
+mm_plugin_nokia_icera_class_init (MMPluginNokiaIceraClass *klass)
+{
+    MMPluginClass *plugin_class = MM_PLUGIN_CLASS (klass);
+
+    plugin_class->create_modem = create_modem;
+    plugin_class->grab_port = grab_port;
+}
---
+++ plugins/nokia/mm-plugin-nokia-icera.h
@@ -0,0 +1,41 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2009 - 2012 Red Hat, Inc.
+ * Copyright (C) 2012 Aleksander Morgado <aleksander@gnu.org>
+ */
+
+#ifndef MM_PLUGIN_NOKIA_ICERA_H
+#define MM_PLUGIN_NOKIA_ICERA_H
+
+#include "mm-plugin.h"
+
+#define MM_TYPE_PLUGIN_NOKIA_ICERA            (mm_plugin_nokia_icera_get_type ())
+#define MM_PLUGIN_NOKIA_ICERA(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), MM_TYPE_PLUGIN_NOKIA_ICERA, MMPluginNokiaIcera))
+#define MM_PLUGIN_NOKIA_ICERA_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  MM_TYPE_PLUGIN_NOKIA_ICERA, MMPluginNokiaIceraClass))
+#define MM_IS_PLUGIN_NOKIA_ICERA(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), MM_TYPE_PLUGIN_NOKIA_ICERA))
+#define MM_IS_PLUGIN_NOKIA_ICERA_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  MM_TYPE_PLUGIN_NOKIA_ICERA))
+#define MM_PLUGIN_NOKIA_ICERA_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  MM_TYPE_PLUGIN_NOKIA_ICERA, MMPluginNokiaIceraClass))
+
+typedef struct {
+    MMPlugin parent;
+} MMPluginNokiaIcera;
+
+typedef struct {
+    MMPluginClass parent;
+} MMPluginNokiaIceraClass;
+
+GType mm_plugin_nokia_icera_get_type (void);
+
+G_MODULE_EXPORT MMPlugin *mm_plugin_create (void);
+
+#endif /* MM_PLUGIN_NOKIA_ICERA_H */
