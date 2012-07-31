e150c4cf922a4caf2b741a2a155e7ae53eb57740
Aleksander Morgado <aleksander@lanedo.com>
2012-07-31 08:52:03 +0200

    novatel: implement allowed mode loading/setting
    Settings are given with the 'AT$NWRAT' arguments as follows:
      1,1: 2G allowed
      2,1: 3G allowed
      0,2: 2G and 3G allowed, none preferred
      1,2: 2G and 3G allowed, 2G preferred
      2,2: 2G and 3G allowed, 3G preferred

--- plugins/novatel/mm-broadband-modem-novatel.c
+++ plugins/novatel/mm-broadband-modem-novatel.c
@@ -25,11 +25,205 @@
 
 #include "ModemManager.h"
 #include "mm-base-modem-at.h"
+#include "mm-iface-modem.h"
 #include "mm-broadband-modem-novatel.h"
 #include "mm-errors-types.h"
+#include "mm-modem-helpers.h"
 #include "mm-log.h"
 
-G_DEFINE_TYPE (MMBroadbandModemNovatel, mm_broadband_modem_novatel, MM_TYPE_BROADBAND_MODEM);
+static void iface_modem_init (MMIfaceModem *iface);
+
+G_DEFINE_TYPE_EXTENDED (MMBroadbandModemNovatel, mm_broadband_modem_novatel, MM_TYPE_BROADBAND_MODEM, 0,
+                        G_IMPLEMENT_INTERFACE (MM_TYPE_IFACE_MODEM, iface_modem_init));
+
+/*****************************************************************************/
+/* Load initial allowed/preferred modes (Modem interface) */
+
+static gboolean
+load_allowed_modes_finish (MMIfaceModem *self,
+                           GAsyncResult *res,
+                           MMModemMode *allowed,
+                           MMModemMode *preferred,
+                           GError **error)
+{
+    const gchar *response;
+    GRegex *r;
+    GMatchInfo *match_info;
+    gint a = -1;
+    gint b = -1;
+
+    response = mm_base_modem_at_command_finish (MM_BASE_MODEM (self), res, error);
+    if (!response)
+        return FALSE;
+
+    g_assert (response != NULL);
+    g_assert (allowed != NULL);
+    g_assert (preferred != NULL);
+
+    r = g_regex_new ("\\$NWRAT:\\s*(\\d),(\\d),(\\d)", G_REGEX_UNGREEDY, 0, NULL);
+    g_assert (r != NULL);
+
+    if (!g_regex_match_full (r, response, strlen (response), 0, 0, &match_info, error)) {
+        g_prefix_error (error,
+                        "Failed to parse mode/tech response '%s': ",
+                        response);
+        g_regex_unref (r);
+        return FALSE;
+    }
+
+    if (!mm_get_int_from_match_info (match_info, 1, &a) ||
+        !mm_get_int_from_match_info (match_info, 2, &b) ||
+        a < 0 || a > 2 ||
+        b < 1 || b > 2) {
+        g_set_error (error,
+                     MM_CORE_ERROR,
+                     MM_CORE_ERROR_FAILED,
+                     "Failed to parse mode/tech response '%s': invalid modes reported",
+                     response);
+        g_match_info_free (match_info);
+        g_regex_unref (r);
+        return FALSE;
+    }
+
+    switch (a) {
+    case 0:
+        *allowed = (MM_MODEM_MODE_2G | MM_MODEM_MODE_3G);
+        *preferred = MM_MODEM_MODE_NONE;
+        break;
+    case 1:
+        if (b == 1) {
+            *allowed = MM_MODEM_MODE_2G;
+            *preferred = MM_MODEM_MODE_NONE;
+        } else /* b == 2 */ {
+            *allowed = (MM_MODEM_MODE_2G | MM_MODEM_MODE_3G);
+            *preferred = MM_MODEM_MODE_2G;
+        }
+        break;
+    case 2:
+        if (b == 1) {
+            *allowed = MM_MODEM_MODE_3G;
+            *preferred = MM_MODEM_MODE_NONE;
+        } else /* b == 2 */ {
+            *allowed = (MM_MODEM_MODE_2G | MM_MODEM_MODE_3G);
+            *preferred = MM_MODEM_MODE_3G;
+        }
+        break;
+    default:
+        /* We only allow mode 0|1|2 */
+        g_assert_not_reached ();
+        break;
+    }
+
+    g_match_info_free (match_info);
+    g_regex_unref (r);
+    return TRUE;
+}
+
+static void
+load_allowed_modes (MMIfaceModem *self,
+                    GAsyncReadyCallback callback,
+                    gpointer user_data)
+{
+    mm_base_modem_at_command (MM_BASE_MODEM (self),
+                              "$NWRAT?",
+                              3,
+                              FALSE,
+                              callback,
+                              user_data);
+}
+
+/*****************************************************************************/
+/* Set allowed modes (Modem interface) */
+
+static gboolean
+set_allowed_modes_finish (MMIfaceModem *self,
+                          GAsyncResult *res,
+                          GError **error)
+{
+    return !g_simple_async_result_propagate_error (G_SIMPLE_ASYNC_RESULT (res), error);
+}
+
+static void
+allowed_mode_update_ready (MMBroadbandModemNovatel *self,
+                           GAsyncResult *res,
+                           GSimpleAsyncResult *operation_result)
+{
+    GError *error = NULL;
+
+    mm_base_modem_at_command_finish (MM_BASE_MODEM (self), res, &error);
+    if (error)
+        /* Let the error be critical. */
+        g_simple_async_result_take_error (operation_result, error);
+    else
+        g_simple_async_result_set_op_res_gboolean (operation_result, TRUE);
+    g_simple_async_result_complete (operation_result);
+    g_object_unref (operation_result);
+}
+
+static void
+set_allowed_modes (MMIfaceModem *self,
+                   MMModemMode allowed,
+                   MMModemMode preferred,
+                   GAsyncReadyCallback callback,
+                   gpointer user_data)
+{
+    GSimpleAsyncResult *result;
+    gchar *command;
+    gint a = -1;
+    gint b = -1;
+
+    result = g_simple_async_result_new (G_OBJECT (self),
+                                        callback,
+                                        user_data,
+                                        set_allowed_modes);
+
+    if (allowed == MM_MODEM_MODE_2G) {
+        a = 1;
+        b = 1;
+    } else if (allowed == MM_MODEM_MODE_3G) {
+        a = 2;
+        b = 1;
+    } else if (allowed == (MM_MODEM_MODE_2G | MM_MODEM_MODE_3G)) {
+        b = 2;
+        if (preferred == MM_MODEM_MODE_NONE)
+            a = 0;
+        else if (preferred == MM_MODEM_MODE_2G)
+            a = 1;
+        else if (preferred == MM_MODEM_MODE_3G)
+            a = 2;
+    }
+
+    if (a < 0 || b < 0) {
+        gchar *allowed_str;
+        gchar *preferred_str;
+
+        allowed_str = mm_modem_mode_build_string_from_mask (allowed);
+        preferred_str = mm_modem_mode_build_string_from_mask (preferred);
+        g_simple_async_result_set_error (result,
+                                         MM_CORE_ERROR,
+                                         MM_CORE_ERROR_FAILED,
+                                         "Requested mode (allowed: '%s', preferred: '%s') not "
+                                         "supported by the modem.",
+                                         allowed_str,
+                                         preferred_str);
+        g_free (allowed_str);
+        g_free (preferred_str);
+
+        g_simple_async_result_complete_in_idle (result);
+        g_object_unref (result);
+        return;
+    }
+
+    command = g_strdup_printf ("AT$NWRAT=%d,%d", a, b);
+    mm_base_modem_at_command (
+        MM_BASE_MODEM (self),
+        command,
+        3,
+        FALSE,
+        (GAsyncReadyCallback)allowed_mode_update_ready,
+        result);
+    g_free (command);
+}
 
 /*****************************************************************************/
 /* Setup ports (Broadband modem class) */
@@ -72,6 +266,15 @@ mm_broadband_modem_novatel_new (const gchar *device,
 static void
 mm_broadband_modem_novatel_init (MMBroadbandModemNovatel *self)
 {
+}
+
+static void
+iface_modem_init (MMIfaceModem *iface)
+{
+    iface->load_allowed_modes = load_allowed_modes;
+    iface->load_allowed_modes_finish = load_allowed_modes_finish;
+    iface->set_allowed_modes = set_allowed_modes;
+    iface->set_allowed_modes_finish = set_allowed_modes_finish;
 }
 
 static void
