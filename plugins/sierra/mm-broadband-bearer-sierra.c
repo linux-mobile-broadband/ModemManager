3666168c121e7ad74c611af74f8c905cec9d030b
Aleksander Morgado <aleksander@lanedo.com>
2012-08-22 16:30:56 +0200

    sierra: new `MMBroadbandBearerSierra' object

---
+++ plugins/sierra/mm-broadband-bearer-sierra.c
@@ -0,0 +1,85 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2008 - 2009 Novell, Inc.
+ * Copyright (C) 2009 - 2012 Red Hat, Inc.
+ * Copyright (C) 2012 Lanedo GmbH
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+#include <ModemManager.h>
+#include <libmm-common.h>
+
+#include "mm-base-modem-at.h"
+#include "mm-broadband-bearer-sierra.h"
+#include "mm-log.h"
+#include "mm-modem-helpers.h"
+#include "mm-utils.h"
+
+G_DEFINE_TYPE (MMBroadbandBearerSierra, mm_broadband_bearer_sierra, MM_TYPE_BROADBAND_BEARER);
+
+/*****************************************************************************/
+
+MMBearer *
+mm_broadband_bearer_sierra_new_finish (GAsyncResult *res,
+                                       GError **error)
+{
+    GObject *bearer;
+    GObject *source;
+
+    source = g_async_result_get_source_object (res);
+    bearer = g_async_initable_new_finish (G_ASYNC_INITABLE (source), res, error);
+    g_object_unref (source);
+
+    if (!bearer)
+        return NULL;
+
+    /* Only export valid bearers */
+    mm_bearer_export (MM_BEARER (bearer));
+
+    return MM_BEARER (bearer);
+}
+
+void
+mm_broadband_bearer_sierra_new (MMBroadbandModemSierra *modem,
+                                MMBearerProperties *config,
+                                GCancellable *cancellable,
+                                GAsyncReadyCallback callback,
+                                gpointer user_data)
+{
+    g_async_initable_new_async (
+        MM_TYPE_BROADBAND_BEARER_SIERRA,
+        G_PRIORITY_DEFAULT,
+        cancellable,
+        callback,
+        user_data,
+        MM_BEARER_MODEM, modem,
+        MM_BEARER_CONFIG, config,
+        NULL);
+}
+
+static void
+mm_broadband_bearer_sierra_init (MMBroadbandBearerSierra *self)
+{
+}
+
+static void
+mm_broadband_bearer_sierra_class_init (MMBroadbandBearerSierraClass *klass)
+{
+}
---
+++ plugins/sierra/mm-broadband-bearer-sierra.h
@@ -0,0 +1,58 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2008 - 2009 Novell, Inc.
+ * Copyright (C) 2009 - 2012 Red Hat, Inc.
+ * Copyright (C) 2012 Lanedo GmbH
+ */
+
+#ifndef MM_BROADBAND_BEARER_SIERRA_H
+#define MM_BROADBAND_BEARER_SIERRA_H
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <libmm-common.h>
+
+#include "mm-broadband-bearer.h"
+#include "mm-broadband-modem-sierra.h"
+
+#define MM_TYPE_BROADBAND_BEARER_SIERRA            (mm_broadband_bearer_sierra_get_type ())
+#define MM_BROADBAND_BEARER_SIERRA(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), MM_TYPE_BROADBAND_BEARER_SIERRA, MMBroadbandBearerSierra))
+#define MM_BROADBAND_BEARER_SIERRA_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  MM_TYPE_BROADBAND_BEARER_SIERRA, MMBroadbandBearerSierraClass))
+#define MM_IS_BROADBAND_BEARER_SIERRA(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), MM_TYPE_BROADBAND_BEARER_SIERRA))
+#define MM_IS_BROADBAND_BEARER_SIERRA_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  MM_TYPE_BROADBAND_BEARER_SIERRA))
+#define MM_BROADBAND_BEARER_SIERRA_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  MM_TYPE_BROADBAND_BEARER_SIERRA, MMBroadbandBearerSierraClass))
+
+typedef struct _MMBroadbandBearerSierra MMBroadbandBearerSierra;
+typedef struct _MMBroadbandBearerSierraClass MMBroadbandBearerSierraClass;
+
+struct _MMBroadbandBearerSierra {
+    MMBroadbandBearer parent;
+};
+
+struct _MMBroadbandBearerSierraClass {
+    MMBroadbandBearerClass parent;
+};
+
+GType mm_broadband_bearer_sierra_get_type (void);
+
+/* Default 3GPP bearer creation implementation */
+void mm_broadband_bearer_sierra_new (MMBroadbandModemSierra *modem,
+                                     MMBearerProperties *config,
+                                     GCancellable *cancellable,
+                                     GAsyncReadyCallback callback,
+                                     gpointer user_data);
+MMBearer *mm_broadband_bearer_sierra_new_finish (GAsyncResult *res,
+                                                 GError **error);
+
+#endif /* MM_BROADBAND_BEARER_SIERRA_H */
