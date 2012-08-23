78a716e07b3fb4e2227eb35ac3cf8d325751805b
Aleksander Morgado <aleksander@lanedo.com>
2012-08-23 15:19:31 +0200

    mbm: implement custom allowed mode setting

--- plugins/mbm/mm-broadband-modem-mbm.c
+++ plugins/mbm/mm-broadband-modem-mbm.c
@@ -140,6 +140,91 @@ load_allowed_modes (MMIfaceModem *self,
 }
 
 /*****************************************************************************/
+/* Set allowed modes (Modem interface) */
+
+static gboolean
+set_allowed_modes_finish (MMIfaceModem *self,
+                          GAsyncResult *res,
+                          GError **error)
+{
+    return !g_simple_async_result_propagate_error (G_SIMPLE_ASYNC_RESULT (res), error);
+}
+
+static void
+allowed_mode_update_ready (MMBaseModem *self,
+                           GAsyncResult *res,
+                           GSimpleAsyncResult *operation_result)
+{
+    GError *error = NULL;
+
+    mm_base_modem_at_command_finish (self, res, &error);
+    if (error)
+        /* Let the error be critical. */
+        g_simple_async_result_take_error (operation_result, error);
+    else
+        g_simple_async_result_set_op_res_gboolean (operation_result, TRUE);
+    g_simple_async_result_complete (operation_result);
+    g_object_unref (operation_result);
+}
+
+static void
+set_allowed_modes (MMIfaceModem *self,
+                   MMModemMode allowed,
+                   MMModemMode preferred,
+                   GAsyncReadyCallback callback,
+                   gpointer user_data)
+{
+    GSimpleAsyncResult *result;
+    gchar *command;
+    gint mbm_mode = -1;
+
+    result = g_simple_async_result_new (G_OBJECT (self),
+                                        callback,
+                                        user_data,
+                                        set_allowed_modes);
+
+    if (allowed == MM_MODEM_MODE_2G)
+        mbm_mode = MBM_NETWORK_MODE_2G;
+    else if (allowed == MM_MODEM_MODE_3G)
+        mbm_mode = MBM_NETWORK_MODE_3G;
+    else if ((allowed == (MM_MODEM_MODE_2G | MM_MODEM_MODE_3G) ||
+              allowed == MM_MODEM_MODE_ANY) &&
+             preferred == MM_MODEM_MODE_NONE)
+        mbm_mode = MBM_NETWORK_MODE_ANY;
+
+    if (mbm_mode < 0) {
+        gchar *allowed_str;
+        gchar *preferred_str;
+
+        allowed_str = mm_modem_mode_build_string_from_mask (allowed);
+        preferred_str = mm_modem_mode_build_string_from_mask (preferred);
+        g_simple_async_result_set_error (result,
+                                         MM_CORE_ERROR,
+                                         MM_CORE_ERROR_FAILED,
+                                         "Requested mode (allowed: '%s', preferred: '%s') not "
+                                         "supported by the modem.",
+                                         allowed_str,
+                                         preferred_str);
+        g_free (allowed_str);
+        g_free (preferred_str);
+
+        g_simple_async_result_complete_in_idle (result);
+        g_object_unref (result);
+        return;
+    }
+
+    command = g_strdup_printf ("+CFUN=%d", mbm_mode);
+    mm_base_modem_at_command (
+        MM_BASE_MODEM (self),
+        command,
+        3,
+        FALSE,
+        (GAsyncReadyCallback)allowed_mode_update_ready,
+        result);
+    g_free (command);
+}
+
+/*****************************************************************************/
 
 MMBroadbandModemMbm *
 mm_broadband_modem_mbm_new (const gchar *device,
@@ -169,6 +254,8 @@ iface_modem_init (MMIfaceModem *iface)
     iface->modem_after_sim_unlock_finish = modem_after_sim_unlock_finish;
     iface->load_allowed_modes = load_allowed_modes;
     iface->load_allowed_modes_finish = load_allowed_modes_finish;
+    iface->set_allowed_modes = set_allowed_modes;
+    iface->set_allowed_modes_finish = set_allowed_modes_finish;
 }
 
 static void
