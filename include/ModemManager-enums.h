24c40e32bd6b5a9becbba05d18f8fbbdc3592d51
Aleksander Morgado <aleksander@lanedo.com>
2012-10-31 12:34:07 +0100

    api: new source for the CDMA Base Station location

--- include/ModemManager-enums.h
+++ include/ModemManager-enums.h
@@ -493,6 +493,7 @@ typedef enum { /*< underscore_name=mm_sms_storage >*/
  * @MM_MODEM_LOCATION_SOURCE_3GPP_LAC_CI: Location Area Code and Cell ID.
  * @MM_MODEM_LOCATION_SOURCE_GPS_RAW: GPS location given by predefined keys.
  * @MM_MODEM_LOCATION_SOURCE_GPS_NMEA: GPS location given as NMEA traces.
+ * @MM_MODEM_LOCATION_SOURCE_CDMA_BS: CDMA base station position.
  *
  * Sources of location information supported by the modem.
  */
@@ -501,6 +502,7 @@ typedef enum { /*< underscore_name=mm_modem_location_source >*/
     MM_MODEM_LOCATION_SOURCE_3GPP_LAC_CI = 1 << 0,
     MM_MODEM_LOCATION_SOURCE_GPS_RAW     = 1 << 1,
     MM_MODEM_LOCATION_SOURCE_GPS_NMEA    = 1 << 2,
+    MM_MODEM_LOCATION_SOURCE_CDMA_BS     = 1 << 3,
 } MMModemLocationSource;
 
 /**
