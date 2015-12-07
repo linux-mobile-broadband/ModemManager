ee85a463cf806cd01a61587520245a4818ab1fbb
Aleksander Morgado <aleksander@aleksander.es>
2015-12-07 13:10:21 +0100

    license: mmcli is GPLv2+, not GPLv3+
    mmcli is GPLv2+; that's what --version has always said and that's what the
    README in ModemManager sources specifies:
      License.
      The ModemManager and mmcli binaries are both GPLv2+.
      The libmm-glib library is LGPLv2+.

--- cli/mmcli-common.h
+++ cli/mmcli-common.h
@@ -1,14 +1,19 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
- * This program is free software; you can redistribute it and/or modify
+ * mmcli -- Control modem status & access information from the command line
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details:
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Copyright (C) 2011 Aleksander Morgado <aleksander@gnu.org>
  */
