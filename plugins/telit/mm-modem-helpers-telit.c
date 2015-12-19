740e1b5461bda9ff28f3ad2210ecf7238c38e785
Aleksander Morgado <aleksander@aleksander.es>
2015-12-19 11:43:17 +0100

    telit: fix minor coding style issues

--- plugins/telit/mm-modem-helpers-telit.c
+++ plugins/telit/mm-modem-helpers-telit.c
@@ -29,7 +29,7 @@
 /*****************************************************************************/
 /* +CSIM response parser */
 
-gint parse_csim_response (const guint step, const gchar* response, GError** error)
+gint parse_csim_response (const guint step, const gchar *response, GError **error)
 {
     GRegex *r = NULL;
     GMatchInfo *match_info = NULL;
