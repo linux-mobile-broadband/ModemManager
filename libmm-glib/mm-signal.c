3047522b2e494776d9aced64180e54c46e861de1
Aleksander Morgado <aleksander@aleksander.es>
2016-08-10 07:46:34 +0200

    api,signal: Ec/Io is in dB, not dBm

--- libmm-glib/mm-signal.c
+++ libmm-glib/mm-signal.c
@@ -82,7 +82,7 @@ mm_signal_set_rssi (MMSignal *self,
  * mm_signal_get_ecio:
  * @self: a #MMSignal.
  *
- * Gets the Ec/Io, in dBm.
+ * Gets the Ec/Io, in dB.
  *
  * Only applicable to CDMA1x, CDMA EV-DO and UMTS (WCDMA).
  *
