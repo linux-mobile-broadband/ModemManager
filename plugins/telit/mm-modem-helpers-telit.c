06bd24e00c97a961490939c33f046cefef01332f
Daniele Palmas <dnlplm@gmail.com>
2016-10-10 14:42:45 +0200

    telit: fix supported and current bands query with GE910
    GE910 is a 2g only modem and when queried for bands it returns
    only a 2g set of bands:
    
    --> 'AT#BND=?<CR>'
    <-- '<CR><LF>#BND: (0-3)<CR><LF><CR><LF>OK<CR><LF>'
    
    Current regex fails, since it considers the 3g bands block mandatory.
    
    A similar problem happens for current bands.
    
    This patch modifies the regular expressions for properly
    supporting GE910 and updates tests.
    
    (cherry picked from commit 6f11ff10cd8da2d431eb9596185ccb577356c6ab)

--- plugins/telit/mm-modem-helpers-telit.c
+++ plugins/telit/mm-modem-helpers-telit.c
@@ -162,9 +162,9 @@ mm_telit_parse_csim_response (const guint step,
     return retries;
 }
 
-#define SUPP_BAND_RESPONSE_REGEX          "#BND:\\s*\\((?P<Bands2G>.*)\\),\\s*\\((?P<Bands3G>.*)\\)"
+#define SUPP_BAND_RESPONSE_REGEX          "#BND:\\s*\\((?P<Bands2G>[0-9\\-,]*)\\)(,\\s*\\((?P<Bands3G>.*)\\))?"
 #define SUPP_BAND_4G_MODEM_RESPONSE_REGEX "#BND:\\s*\\((?P<Bands2G>.*)\\),\\s*\\((?P<Bands3G>.*)\\),\\s*\\((?P<Bands4G>\\d+-\\d+)\\)"
-#define CURR_BAND_RESPONSE_REGEX          "#BND:\\s*(?P<Bands2G>\\d+),\\s*(?P<Bands3G>\\d+)"
+#define CURR_BAND_RESPONSE_REGEX          "#BND:\\s*(?P<Bands2G>\\d+)(,\\s*(?P<Bands3G>\\d+))?"
 #define CURR_BAND_4G_MODEM_RESPONSE_REGEX "#BND:\\s*(?P<Bands2G>\\d+),\\s*(?P<Bands3G>\\d+),\\s*(?P<Bands4G>\\d+)"
 
 /*****************************************************************************/
