4d6896f0d919cc0e474a575f6a809ad5f7a121e1
Aleksander Morgado <aleksander@aleksander.es>
2017-06-22 10:51:40 +0200

    build: rename --with-suspend-resume to --with-systemd-suspend-resume
    And only define WITH_SYSTEMD_SUSPEND_RESUME when enabled.
    
    We already have other systemd specific features under the
    --with-systemd-* name scheme, so consolidate that, given that we don't
    have any other method to support suspend/resume notifications.

--- src/main.c
+++ src/main.c
@@ -30,7 +30,7 @@
 #include "mm-log.h"
 #include "mm-context.h"
 
-#if WITH_SUSPEND_RESUME
+#if defined WITH_SYSTEMD_SUSPEND_RESUME
 # include "mm-sleep-monitor.h"
 #endif
 
@@ -55,7 +55,7 @@ quit_cb (gpointer user_data)
     return FALSE;
 }
 
-#if WITH_SUSPEND_RESUME
+#if defined WITH_SYSTEMD_SUSPEND_RESUME
 
 static void
 sleeping_cb (MMSleepMonitor *sleep_monitor)
@@ -165,7 +165,7 @@ main (int argc, char *argv[])
                               name_lost_cb,
                               NULL,
                               NULL);
-#if WITH_SUSPEND_RESUME
+#if defined WITH_SYSTEMD_SUSPEND_RESUME
     {
         MMSleepMonitor *sleep_monitor;
 
