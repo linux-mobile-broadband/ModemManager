3bc48b21302958f0f39866e699c187f2f96f2f21
Aleksander Morgado <aleksander@aleksander.es>
2017-07-17 09:57:21 +0200

    via: fix async completion in cdma event handlers setting
    (cherry picked from commit 24e8785a83eb9ac88a36b58c0c5ba83a4689800b)

--- plugins/via/mm-broadband-modem-via.c
+++ plugins/via/mm-broadband-modem-via.c
@@ -355,7 +355,7 @@ parent_cdma_setup_unsolicited_events_ready (MMIfaceModemCdma *self,
     else {
         /* Our own setup now */
         set_unsolicited_events_handlers (MM_BROADBAND_MODEM_VIA (self), TRUE);
-        g_simple_async_result_set_op_res_gboolean (G_SIMPLE_ASYNC_RESULT (res), TRUE);
+        g_simple_async_result_set_op_res_gboolean (simple, TRUE);
     }
 
     g_simple_async_result_complete (simple);
@@ -391,7 +391,7 @@ parent_cdma_cleanup_unsolicited_events_ready (MMIfaceModemCdma *self,
     if (!iface_modem_cdma_parent->cleanup_unsolicited_events_finish (self, res, &error))
         g_simple_async_result_take_error (simple, error);
     else
-        g_simple_async_result_set_op_res_gboolean (G_SIMPLE_ASYNC_RESULT (res), TRUE);
+        g_simple_async_result_set_op_res_gboolean (simple, TRUE);
     g_simple_async_result_complete (simple);
     g_object_unref (simple);
 }
