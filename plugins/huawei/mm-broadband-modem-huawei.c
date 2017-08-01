68075a23a7eb36d4bfc8ec5eb6af473aaecd66c4
Ben Chan <benchan@chromium.org>
2017-08-01 11:48:57 -0700

    huawei: port modem_voice_enable_unsolicited_events to use GTask

--- plugins/huawei/mm-broadband-modem-huawei.c
+++ plugins/huawei/mm-broadband-modem-huawei.c
@@ -3125,23 +3125,22 @@ modem_voice_enable_unsolicited_events_finish (MMIfaceModemVoice *self,
                                               GAsyncResult *res,
                                               GError **error)
 {
-    return !g_simple_async_result_propagate_error (G_SIMPLE_ASYNC_RESULT (res), error);
+    return g_task_propagate_boolean (G_TASK (res), error);
 }
 
 static void
 own_voice_enable_unsolicited_events_ready (MMBaseModem *self,
                                            GAsyncResult *res,
-                                           GSimpleAsyncResult *simple)
+                                           GTask *task)
 {
     GError *error = NULL;
 
     mm_base_modem_at_sequence_full_finish (self, res, NULL, &error);
     if (error)
-        g_simple_async_result_take_error (simple, error);
+        g_task_return_error (task, error);
     else
-        g_simple_async_result_set_op_res_gboolean (simple, TRUE);
-    g_simple_async_result_complete (simple);
-    g_object_unref (simple);
+        g_task_return_boolean (task, TRUE);
+    g_object_unref (task);
 }
 
 static const MMBaseModemAtCommand unsolicited_voice_enable_sequence[] = {
@@ -3153,14 +3152,13 @@ static const MMBaseModemAtCommand unsolicited_voice_enable_sequence[] = {
 static void
 parent_voice_enable_unsolicited_events_ready (MMIfaceModemVoice *self,
                                               GAsyncResult *res,
-                                              GSimpleAsyncResult *simple)
+                                              GTask *task)
 {
     GError *error = NULL;
 
     if (!iface_modem_voice_parent->enable_unsolicited_events_finish (self, res, &error)) {
-        g_simple_async_result_take_error (simple, error);
-        g_simple_async_result_complete (simple);
-        g_object_unref (simple);
+        g_task_return_error (task, error);
+        g_object_unref (task);
         return;
     }
 
@@ -3173,7 +3171,7 @@ parent_voice_enable_unsolicited_events_ready (MMIfaceModemVoice *self,
         NULL, /* response_processor_context_free */
         NULL, /* cancellable */
         (GAsyncReadyCallback)own_voice_enable_unsolicited_events_ready,
-        simple);
+        task);
 }
 
 static void
@@ -3181,18 +3179,15 @@ modem_voice_enable_unsolicited_events (MMIfaceModemVoice *self,
                                        GAsyncReadyCallback callback,
                                        gpointer user_data)
 {
-    GSimpleAsyncResult *result;
+    GTask *task;
 
-    result = g_simple_async_result_new (G_OBJECT (self),
-                                        callback,
-                                        user_data,
-                                        modem_voice_enable_unsolicited_events);
+    task = g_task_new (self, NULL, callback, user_data);
 
     /* Chain up parent's enable */
     iface_modem_voice_parent->enable_unsolicited_events (
         self,
         (GAsyncReadyCallback)parent_voice_enable_unsolicited_events_ready,
-        result);
+        task);
 }
 
 /*****************************************************************************/
