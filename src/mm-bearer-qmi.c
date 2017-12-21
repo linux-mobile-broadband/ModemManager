850107acbfe3878326b695d4ea31f02d2321aa22
Aleksander Morgado <aleksander@aleksander.es>
2017-12-21 14:43:08 +0100

    bearer-qmi: fix logic if error setting up WDS event report indications
    If we get an error when setting up the WDS event report indications,
    make sure we run connect_context_step() after having set the next step
    as CONNECT_STEP_LAST.

--- src/mm-bearer-qmi.c
+++ src/mm-bearer-qmi.c
@@ -1058,12 +1058,12 @@ connect_enable_indications_ipv4_ready (QmiClientWds *client,
 
     ctx->event_report_ipv4_indication_id =
         connect_enable_indications_ready (client, res, ctx->self, &ctx->error_ipv4);
-    if (!ctx->event_report_ipv4_indication_id) {
-        ctx->step = CONNECT_STEP_LAST;
-        return;
-    }
 
-    ctx->step++;
+    if (!ctx->event_report_ipv4_indication_id)
+        ctx->step = CONNECT_STEP_LAST;
+    else
+        ctx->step++;
+
     connect_context_step (task);
 }
 
@@ -1079,12 +1079,12 @@ connect_enable_indications_ipv6_ready (QmiClientWds *client,
 
     ctx->event_report_ipv6_indication_id =
         connect_enable_indications_ready (client, res, ctx->self, &ctx->error_ipv6);
-    if (!ctx->event_report_ipv6_indication_id) {
-        ctx->step = CONNECT_STEP_LAST;
-        return;
-    }
 
-    ctx->step++;
+    if (!ctx->event_report_ipv6_indication_id)
+        ctx->step = CONNECT_STEP_LAST;
+    else
+        ctx->step++;
+
     connect_context_step (task);
 }
 
