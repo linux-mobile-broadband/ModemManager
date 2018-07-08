9b0cd6aaf529c3d12961e86c0496a52ac17ab5c1
Aleksander Morgado <aleksander@aleksander.es>
2018-07-08 14:25:01 +0200

    broadband-modem-qmi: plug memleak when processing a factory reset
    (cherry picked from commit d3bc04676444b26991dd08426422079f541ba8f4)

--- src/mm-broadband-modem-qmi.c
+++ src/mm-broadband-modem-qmi.c
@@ -3489,6 +3489,7 @@ modem_factory_reset (MMIfaceModem *self,
                                              NULL,
                                              (GAsyncReadyCallback)dms_restore_factory_defaults_ready,
                                              result);
+    qmi_message_dms_restore_factory_defaults_input_unref (input);
 }
 
 /*****************************************************************************/
