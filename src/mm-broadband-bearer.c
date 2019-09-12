06b6d0ce100a55682a2ce47b33f78b2689a90738
Matthew Starr <mstarr@hedonline.com>
2019-09-12 11:23:49 -0500

    mm-broadband-bearer: Increase CGACT timeout on deactivation
    On u-blox modems it has been observed that attempting to force down the
    bearer when the modem had an active data connection, lost the connection
    but the bearer is still up, and the modem is searching for the network,
    ModemManager would timeout before the CGACT command could return.  This
    can put the modem in a state where the ModemManager state does not match
    the modem's actual state of being connected.
    
    When deactivating the current active bearer, the timeout has been
    changed from 10 seconds to 45 seconds.  u-blox modems can take up to 40
    seconds to respond to deactivation requests through the AT+CGACT
    command according to the u-blox AT command reference.

--- src/mm-broadband-bearer.c
+++ src/mm-broadband-bearer.c
@@ -1692,7 +1692,7 @@ disconnect_3gpp (MMBroadbandBearer *self,
         mm_base_modem_at_command_full (ctx->modem,
                                        ctx->primary,
                                        ctx->cgact_command,
-                                       10,
+                                       45,
                                        FALSE,
                                        FALSE, /* raw */
                                        NULL, /* cancellable */
@@ -1711,7 +1711,7 @@ disconnect_3gpp (MMBroadbandBearer *self,
         mm_base_modem_at_command_full (ctx->modem,
                                        ctx->secondary,
                                        ctx->cgact_command,
-                                       10,
+                                       45,
                                        FALSE,
                                        FALSE, /* raw */
                                        NULL, /* cancellable */
