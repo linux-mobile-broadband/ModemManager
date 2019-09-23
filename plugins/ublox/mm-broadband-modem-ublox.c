dcf50efdd51e64e7a6bf3a65986434aa9a35d0bb
Aleksander Morgado <aleksander@aleksander.es>
2019-09-23 18:08:38 +0200

    ublox: fix calling setup_unsolicited_events_finish()

--- plugins/ublox/mm-broadband-modem-ublox.c
+++ plugins/ublox/mm-broadband-modem-ublox.c
@@ -1208,8 +1208,8 @@ parent_voice_setup_unsolicited_events_ready (MMIfaceModemVoice *self,
 {
     GError  *error = NULL;
 
-    if (!iface_modem_voice_parent->cleanup_unsolicited_events_finish (self, res, &error)) {
-        mm_warn ("Couldn't cleanup parent voice unsolicited events: %s", error->message);
+    if (!iface_modem_voice_parent->setup_unsolicited_events_finish (self, res, &error)) {
+        mm_warn ("Couldn't setup parent voice unsolicited events: %s", error->message);
         g_error_free (error);
     }
 
