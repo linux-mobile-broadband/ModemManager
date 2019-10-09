c0e1bde03ee1aef418644c515060b525872af436
Aleksander Morgado <aleksander@aleksander.es>
2019-10-09 10:48:47 +0200

    base-modem-at: avoid using MM_CORE_ERROR_CANCELLED

--- src/mm-base-modem-at.c
+++ src/mm-base-modem-at.c
@@ -168,9 +168,7 @@ at_sequence_parse_response (MMPortSerialAt *port,
 
     /* Cancelled? */
     if (g_cancellable_is_cancelled (ctx->cancellable)) {
-        g_simple_async_result_set_error (ctx->simple,
-                                         MM_CORE_ERROR,
-                                         MM_CORE_ERROR_CANCELLED,
+        g_simple_async_result_set_error (ctx->simple, G_IO_ERROR, G_IO_ERROR_CANCELLED,
                                          "AT sequence was cancelled");
         if (error)
             g_error_free (error);
@@ -483,9 +481,7 @@ at_command_ready (MMPortSerialAt *port,
 
     /* Cancelled? */
     if (g_cancellable_is_cancelled (ctx->cancellable)) {
-        g_simple_async_result_set_error (ctx->result,
-                                         MM_CORE_ERROR,
-                                         MM_CORE_ERROR_CANCELLED,
+        g_simple_async_result_set_error (ctx->result, G_IO_ERROR, G_IO_ERROR_CANCELLED,
                                          "AT command was cancelled");
         if (error)
             g_error_free (error);
