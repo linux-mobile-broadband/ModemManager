8fcc470a5cdea2987536df9436e99a19a58473eb
Aleksander Morgado <aleksander@aleksander.es>
2019-11-17 13:50:42 +0100

    plugin-manager: always make sure all needed symbols are available
    Never do lazy loading, we'll always make sure that a plugin will only
    be fully loaded if all the symbols it requires are already available.

--- src/mm-plugin-manager.c
+++ src/mm-plugin-manager.c
@@ -1599,7 +1599,7 @@ load_plugin (const gchar *path)
     /* Get printable UTF-8 string of the path */
     path_display = g_filename_display_name (path);
 
-    module = g_module_open (path, G_MODULE_BIND_LAZY);
+    module = g_module_open (path, 0);
     if (!module) {
         mm_warn ("[plugin manager] could not load plugin '%s': %s", path_display, g_module_error ());
         goto out;
@@ -1660,7 +1660,7 @@ load_shared (const gchar *path)
     /* Get printable UTF-8 string of the path */
     path_display = g_filename_display_name (path);
 
-    module = g_module_open (path, G_MODULE_BIND_LAZY);
+    module = g_module_open (path, 0);
     if (!module) {
         mm_warn ("[plugin manager] could not load shared '%s': %s", path_display, g_module_error ());
         goto out;
