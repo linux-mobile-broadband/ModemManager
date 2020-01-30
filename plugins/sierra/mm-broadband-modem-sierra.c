54a6e9fc85cb4c1b46937fdc6481e7b88b8dba2b
Aleksander Morgado <aleksander@aleksander.es>
2020-01-30 18:29:43 +0100

    sierra: fix warnings with -Wdiscarded-qualifiers
      sierra/mm-broadband-modem-sierra.c:1815:7: error: initialization discards ‘const’ qualifier from pointer target type [-Werror=discarded-qualifiers]
       1815 |     { "!TIME?", 3, FALSE, parse_time_reply },    /* 3GPP */
            |       ^~~~~~~~
      sierra/mm-broadband-modem-sierra.c:1816:7: error: initialization discards ‘const’ qualifier from pointer target type [-Werror=discarded-qualifiers]
       1816 |     { "!SYSTIME?", 3, FALSE, parse_time_reply }, /* CDMA */
            |       ^~~~~~~~~~~

--- plugins/sierra/mm-broadband-modem-sierra.c
+++ plugins/sierra/mm-broadband-modem-sierra.c
@@ -1812,8 +1812,8 @@ parse_time_reply (MMBaseModem *self,
 }
 
 static const MMBaseModemAtCommand time_check_sequence[] = {
-    { "!TIME?", 3, FALSE, parse_time_reply },    /* 3GPP */
-    { "!SYSTIME?", 3, FALSE, parse_time_reply }, /* CDMA */
+    { (gchar *) "!TIME?", 3, FALSE, parse_time_reply },    /* 3GPP */
+    { (gchar *) "!SYSTIME?", 3, FALSE, parse_time_reply }, /* CDMA */
     { NULL }
 };
 
