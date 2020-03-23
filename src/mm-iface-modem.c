78266ac4895e34a02f4f66893d7ae867b38864b0
Aleksander Morgado <aleksander@aleksander.es>
2020-03-23 07:02:57 +0100

    iface-modem: simplify is_cdma_only() check
    The check needs to look for a matching single bit, so no need to make
    it too complex.

--- src/mm-iface-modem.c
+++ src/mm-iface-modem.c
@@ -5576,10 +5576,7 @@ mm_iface_modem_is_3gpp_lte_only (MMIfaceModem *self)
 gboolean
 mm_iface_modem_is_cdma_only (MMIfaceModem *self)
 {
-    MMModemCapability capabilities;
-
-    capabilities = mm_iface_modem_get_current_capabilities (self);
-    return (capabilities & MM_MODEM_CAPABILITY_CDMA_EVDO) && !((MM_MODEM_CAPABILITY_CDMA_EVDO ^ capabilities) & capabilities);
+    return (mm_iface_modem_get_current_capabilities (self) == MM_MODEM_CAPABILITY_CDMA_EVDO);
 }
 
 /*****************************************************************************/
