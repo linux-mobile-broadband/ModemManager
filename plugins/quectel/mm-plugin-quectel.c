e623a54d274eea6803043c3ace4168b5ad599ae0
Aleksander Morgado <aleksander@aleksander.es>
2020-04-07 15:55:40 +0200

    quectel: port to use object logging

--- plugins/quectel/mm-plugin-quectel.c
+++ plugins/quectel/mm-plugin-quectel.c
@@ -19,7 +19,7 @@
 #define _LIBMM_INSIDE_MM
 #include <libmm-glib.h>
 
-#include "mm-log.h"
+#include "mm-log-object.h"
 #include "mm-plugin-quectel.h"
 #include "mm-broadband-modem-quectel.h"
 
@@ -45,7 +45,7 @@ create_modem (MMPlugin     *self,
 {
 #if defined WITH_QMI
     if (mm_port_probe_list_has_qmi_port (probes)) {
-        mm_dbg ("QMI-powered Quectel modem found...");
+        mm_obj_dbg (self, "QMI-powered Quectel modem found...");
         return MM_BASE_MODEM (mm_broadband_modem_qmi_quectel_new (uid,
                                                                   drivers,
                                                                   mm_plugin_get_name (self),
