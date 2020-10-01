97a1a763792967c5581dcd9fe8fcb8558f3d4784
Daniele Palmas <dnlplm@gmail.com>
2020-10-01 13:19:28 +0200

    port-qmi: increase qmi_device_open retries
    Telit FN980 requires more time for becoming responsive to
    qmi requests after device appearance.
    
    (cherry picked from commit 075bcb74f30d18985e1ada82312fe58215f82040)

--- src/mm-port-qmi.c
+++ src/mm-port-qmi.c
@@ -509,7 +509,7 @@ port_open_step (GTask *task)
         qmi_device_open (ctx->device,
                          (QMI_DEVICE_OPEN_FLAGS_VERSION_INFO |
                           QMI_DEVICE_OPEN_FLAGS_PROXY),
-                         20,
+                         25,
                          g_task_get_cancellable (task),
                          (GAsyncReadyCallback) qmi_device_open_first_ready,
                          task);
