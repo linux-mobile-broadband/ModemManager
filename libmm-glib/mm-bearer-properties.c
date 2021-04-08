ab6033e9582ec79a8e0c0a7735a4dbad0e3f8a6b
Aleksander Morgado <aleksander@aleksander.es>
2021-04-08 22:40:20 +0200

    libmm-glib,bearer-properties: fix usage of CMP_FLAGS_NO_RM_PROTOCOL
    (cherry picked from commit 3d665d22093b985f6860170bca8d33a5562056d1)

--- libmm-glib/mm-bearer-properties.c
+++ libmm-glib/mm-bearer-properties.c
@@ -764,8 +764,10 @@ mm_bearer_properties_cmp (MMBearerProperties         *a,
         if (a->priv->allow_roaming_set != b->priv->allow_roaming_set)
             return FALSE;
     }
-    if (a->priv->rm_protocol != b->priv->rm_protocol)
-        return FALSE;
+    if (!(flags & MM_BEARER_PROPERTIES_CMP_FLAGS_NO_RM_PROTOCOL)) {
+        if (a->priv->rm_protocol != b->priv->rm_protocol)
+            return FALSE;
+    }
     return TRUE;
 }
 
