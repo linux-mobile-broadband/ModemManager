4ae623c37b5eb395c79b49a044958c09fd6d23a7
Aleksander Morgado <aleksander@aleksander.es>
2021-04-21 11:33:53 +0200

    build: disable gtkdocize in autoreconf
    We have a custom modified gtk-doc.make file in our sources, which
    disables the gtkdoc-rebase on local install step, so we don't want
    autoreconf to copy a different one when we're bootstrapping.

--- autogen.sh
+++ autogen.sh
@@ -14,7 +14,7 @@ PKG_NAME=ModemManager
 }
 
 (cd $srcdir;
-    autoreconf --force --install --verbose
+    GTKDOCIZE="true" autoreconf --force --install --verbose
 )
 
 if test -z "$NOCONFIGURE"; then
