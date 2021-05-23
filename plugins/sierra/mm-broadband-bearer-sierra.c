c188ba3c159309d4ab1b3715d83a1624426a2c5a
Aleksander Morgado <aleksander@aleksander.es>
2021-05-23 22:17:25 +0200

    sierra: reuse the same method for load and reload
    The load_() method is used for connection monitoring; while the
    reload_() method is used to sync connection status after a
    suspend/resume operation. The same method can be used for both things
    in the Sierra plugin.

--- plugins/sierra/mm-broadband-bearer-sierra.c
+++ plugins/sierra/mm-broadband-bearer-sierra.c
@@ -663,6 +663,10 @@ mm_broadband_bearer_sierra_class_init (MMBroadbandBearerSierraClass *klass)
 
     base_bearer_class->load_connection_status = load_connection_status;
     base_bearer_class->load_connection_status_finish = load_connection_status_finish;
+#if defined WITH_SYSTEMD_SUSPEND_RESUME
+    base_bearer_class->reload_connection_status = load_connection_status;
+    base_bearer_class->reload_connection_status_finish = load_connection_status_finish;
+#endif
 
     broadband_bearer_class->dial_3gpp = dial_3gpp;
     broadband_bearer_class->dial_3gpp_finish = dial_3gpp_finish;
