e716a306b1a6cc654523f6b911f15213d85acc85
Aleksander Morgado <aleksander@aleksander.es>
2021-05-23 15:44:22 +0200

    iface-modem-3gpp: ignore cancellation during sync()
    There is no input cancellable in the method, so the GTask will never
    get cancelled from the outside.

--- src/mm-iface-modem-3gpp.c
+++ src/mm-iface-modem-3gpp.c
@@ -2600,12 +2600,6 @@ interface_syncing_step (GTask *task)
     MMIfaceModem3gpp *self;
     SyncingContext   *ctx;
 
-    /* Don't run new steps if we're cancelled */
-    if (g_task_return_error_if_cancelled (task)) {
-        g_object_unref (task);
-        return;
-    }
-
     self = g_task_get_source_object (task);
     ctx = g_task_get_task_data (task);
 
