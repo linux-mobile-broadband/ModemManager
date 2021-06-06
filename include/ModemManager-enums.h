40c647ce3e88e646e00a0990340278b941aec242
Aleksander Morgado <aleksander@aleksander.es>
2021-06-06 12:15:56 +0200

    Revert "api: QDU update method defined in MM 1.16.6 already"
    This reverts commit 6ffe84a122b9a79a8f0951af5ee5075f16726bdd.

--- include/ModemManager-enums.h
+++ include/ModemManager-enums.h
@@ -1515,7 +1515,7 @@ typedef enum { /*< underscore_name=mm_call_direction >*/
  * @MM_MODEM_FIRMWARE_UPDATE_METHOD_NONE: No method specified.
  * @MM_MODEM_FIRMWARE_UPDATE_METHOD_FASTBOOT: Device supports fastboot-based update.
  * @MM_MODEM_FIRMWARE_UPDATE_METHOD_QMI_PDC: Device supports QMI PDC based update.
- * @MM_MODEM_FIRMWARE_UPDATE_METHOD_MBIM_QDU: Device supports MBIM QDU based update. Since 1.16.6.
+ * @MM_MODEM_FIRMWARE_UPDATE_METHOD_MBIM_QDU: Device supports MBIM QDU based update. Since 1.18.
  *
  * Type of firmware update method supported by the module.
  *
