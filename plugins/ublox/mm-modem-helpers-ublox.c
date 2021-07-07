linux-mobile-broadband/ModemManager

commit 437fb830c8071c3eca1de5f14378a269f48476f9

ublox,helpers: assume all SARA/LARA devices require COPS

plugins/ublox/mm-modem-helpers-ublox.c
@@ -835,66 +835,66 @@ static const BandConfiguration band_configuration[] = {
     },
     {
         .model    = "LARA-R3121",
-        .method   = SETTINGS_UPDATE_METHOD_UNKNOWN,
-        .uact     = FEATURE_SUPPORT_UNKNOWN,
-        .ubandsel = FEATURE_SUPPORT_UNKNOWN,
+        .method   = SETTINGS_UPDATE_METHOD_COPS,
+        .uact     = FEATURE_UNSUPPORTED,
+        .ubandsel = FEATURE_SUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_3, MM_MODEM_BAND_EUTRAN_7, MM_MODEM_BAND_EUTRAN_20 }
     },
     {
         .model    = "SARA-N200",
-        .method   = SETTINGS_UPDATE_METHOD_UNKNOWN,
-        .uact     = FEATURE_SUPPORT_UNKNOWN,
-        .ubandsel = FEATURE_SUPPORT_UNKNOWN,
+        .method   = SETTINGS_UPDATE_METHOD_COPS,
+        .uact     = FEATURE_UNSUPPORTED,
+        .ubandsel = FEATURE_SUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_8 }
     },
     {
         .model    = "SARA-N201",
-        .method   = SETTINGS_UPDATE_METHOD_UNKNOWN,
-        .uact     = FEATURE_SUPPORT_UNKNOWN,
-        .ubandsel = FEATURE_SUPPORT_UNKNOWN,
+        .method   = SETTINGS_UPDATE_METHOD_COPS,
+        .uact     = FEATURE_UNSUPPORTED,
+        .ubandsel = FEATURE_SUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_5 }
     },
     {
         .model    = "SARA-N210",
-        .method   = SETTINGS_UPDATE_METHOD_UNKNOWN,
-        .uact     = FEATURE_SUPPORT_UNKNOWN,
-        .ubandsel = FEATURE_SUPPORT_UNKNOWN,
+        .method   = SETTINGS_UPDATE_METHOD_COPS,
+        .uact     = FEATURE_UNSUPPORTED,
+        .ubandsel = FEATURE_SUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_20 }
     },
     {
         .model    = "SARA-N211",
-        .method   = SETTINGS_UPDATE_METHOD_UNKNOWN,
-        .uact     = FEATURE_SUPPORT_UNKNOWN,
-        .ubandsel = FEATURE_SUPPORT_UNKNOWN,
+        .method   = SETTINGS_UPDATE_METHOD_COPS,
+        .uact     = FEATURE_UNSUPPORTED,
+        .ubandsel = FEATURE_SUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_8, MM_MODEM_BAND_EUTRAN_20 }
     },
     {
         .model    = "SARA-N280",
-        .method   = SETTINGS_UPDATE_METHOD_UNKNOWN,
-        .uact     = FEATURE_SUPPORT_UNKNOWN,
-        .ubandsel = FEATURE_SUPPORT_UNKNOWN,
+        .method   = SETTINGS_UPDATE_METHOD_COPS,
+        .uact     = FEATURE_UNSUPPORTED,
+        .ubandsel = FEATURE_SUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_28 }
     },
     {
         .model    = "SARA-R410M-52B",
-        .method   = SETTINGS_UPDATE_METHOD_UNKNOWN,
-        .uact     = FEATURE_SUPPORT_UNKNOWN,
-        .ubandsel = FEATURE_SUPPORT_UNKNOWN,
+        .method   = SETTINGS_UPDATE_METHOD_COPS,
+        .uact     = FEATURE_UNSUPPORTED,
+        .ubandsel = FEATURE_SUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_2,  MM_MODEM_BAND_EUTRAN_4, MM_MODEM_BAND_EUTRAN_5,
                       MM_MODEM_BAND_EUTRAN_12, MM_MODEM_BAND_EUTRAN_13 }
     },
     {
         .model    = "SARA-R410M-02B",
-        .method   = SETTINGS_UPDATE_METHOD_UNKNOWN,
-        .uact     = FEATURE_SUPPORT_UNKNOWN,
-        .ubandsel = FEATURE_SUPPORT_UNKNOWN,
+        .method   = SETTINGS_UPDATE_METHOD_COPS,
+        .uact     = FEATURE_UNSUPPORTED,
+        .ubandsel = FEATURE_SUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_1,  MM_MODEM_BAND_EUTRAN_2, MM_MODEM_BAND_EUTRAN_3,
                       MM_MODEM_BAND_EUTRAN_4, MM_MODEM_BAND_EUTRAN_5, MM_MODEM_BAND_EUTRAN_8,
@@ -903,9 +903,9 @@ static const BandConfiguration band_configuration[] = {
     },
     {
         .model    = "SARA-R412M-02B",
-        .method   = SETTINGS_UPDATE_METHOD_UNKNOWN,
-        .uact     = FEATURE_SUPPORT_UNKNOWN,
-        .ubandsel = FEATURE_SUPPORT_UNKNOWN,
+        .method   = SETTINGS_UPDATE_METHOD_COPS,
+        .uact     = FEATURE_UNSUPPORTED,
+        .ubandsel = FEATURE_SUPPORTED,
         .mode     = MM_MODEM_MODE_2G | MM_MODEM_MODE_4G,
         .bands_2g = { MM_MODEM_BAND_G850, MM_MODEM_BAND_EGSM, MM_MODEM_BAND_DCS, MM_MODEM_BAND_PCS },
         .bands_4g = { MM_MODEM_BAND_EUTRAN_1,  MM_MODEM_BAND_EUTRAN_2,  MM_MODEM_BAND_EUTRAN_3,
@@ -915,9 +915,9 @@ static const BandConfiguration band_configuration[] = {
     },
     {
         .model    = "SARA-N410-02B",
-        .method   = SETTINGS_UPDATE_METHOD_UNKNOWN,
-        .uact     = FEATURE_SUPPORT_UNKNOWN,
-        .ubandsel = FEATURE_SUPPORT_UNKNOWN,
+        .method   = SETTINGS_UPDATE_METHOD_COPS,
+        .uact     = FEATURE_UNSUPPORTED,
+        .ubandsel = FEATURE_SUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_1,  MM_MODEM_BAND_EUTRAN_2,  MM_MODEM_BAND_EUTRAN_3,
                       MM_MODEM_BAND_EUTRAN_4,  MM_MODEM_BAND_EUTRAN_5,  MM_MODEM_BAND_EUTRAN_8,

commit 266e98e0204bf170fb497d07323e377aeb005631

ublox: Mark ubandsel as unsupported on SARA-R4 and -N4
See log from a SARA-R410M-02B-01 before the change:

    Feb 01 05:40:01 unit ModemManager[304]: <debug> [1612158001.012330] [modem0/ttymxc4/at] --> 'AT+UBANDSEL?<CR>'
    Feb 01 05:40:01 unit ModemManager[304]: <debug> [1612158001.026831] [modem0/ttymxc4/at] <-- '<CR><LF>ERROR<CR><LF>'
    Feb 01 05:40:01 unit ModemManager[304]: <debug> [1612158001.027113] [modem0/ttymxc4/at] operation failure: 100 (Unknown error)
    Feb 01 05:40:01 unit ModemManager[304]: <warn>  [1612158001.027298] [modem0] couldn't load current bands: Unknown error

Backed by SARA-R4 series AT commands manual, no reference to +UBANDSEL
in the manual at all.

Log after the change:

    Feb 01 06:58:25 unit ModemManager[329]: <debug> [1612162705.500845] [modem0] (u-blox) support configuration found for 'SARA-R410M-02B'
    Feb 01 06:58:25 unit ModemManager[329]: <debug> [1612162705.500961] [modem0] (u-blox)   band update requires explicit unregistration
    Feb 01 06:58:25 unit ModemManager[329]: <debug> [1612162705.501052] [modem0] (u-blox)   UACT based band configuration unsupported
    Feb 01 06:58:25 unit ModemManager[329]: <debug> [1612162705.501141] [modem0] (u-blox)   UBANDSEL based band configuration unsupported

Fixes: 437fb830c807 ("ublox,helpers: assume all SARA/LARA devices require COPS")
Signed-off-by: Alexander Dahl <ada@thorsis.com>

plugins/ublox/mm-modem-helpers-ublox.c
@@ -888,7 +888,7 @@ static const BandConfiguration band_configuration[] = {
         .model    = "SARA-R410M-52B",
         .method   = SETTINGS_UPDATE_METHOD_COPS,
         .uact     = FEATURE_UNSUPPORTED,
-        .ubandsel = FEATURE_SUPPORTED,
+        .ubandsel = FEATURE_UNSUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_2,  MM_MODEM_BAND_EUTRAN_4, MM_MODEM_BAND_EUTRAN_5,
                       MM_MODEM_BAND_EUTRAN_12, MM_MODEM_BAND_EUTRAN_13 }
@@ -897,7 +897,7 @@ static const BandConfiguration band_configuration[] = {
         .model    = "SARA-R410M-02B",
         .method   = SETTINGS_UPDATE_METHOD_COPS,
         .uact     = FEATURE_UNSUPPORTED,
-        .ubandsel = FEATURE_SUPPORTED,
+        .ubandsel = FEATURE_UNSUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_1,  MM_MODEM_BAND_EUTRAN_2, MM_MODEM_BAND_EUTRAN_3,
                       MM_MODEM_BAND_EUTRAN_4, MM_MODEM_BAND_EUTRAN_5, MM_MODEM_BAND_EUTRAN_8,
@@ -908,7 +908,7 @@ static const BandConfiguration band_configuration[] = {
         .model    = "SARA-R412M-02B",
         .method   = SETTINGS_UPDATE_METHOD_COPS,
         .uact     = FEATURE_UNSUPPORTED,
-        .ubandsel = FEATURE_SUPPORTED,
+        .ubandsel = FEATURE_UNSUPPORTED,
         .mode     = MM_MODEM_MODE_2G | MM_MODEM_MODE_4G,
         .bands_2g = { MM_MODEM_BAND_G850, MM_MODEM_BAND_EGSM, MM_MODEM_BAND_DCS, MM_MODEM_BAND_PCS },
         .bands_4g = { MM_MODEM_BAND_EUTRAN_1,  MM_MODEM_BAND_EUTRAN_2,  MM_MODEM_BAND_EUTRAN_3,
@@ -920,7 +920,7 @@ static const BandConfiguration band_configuration[] = {
         .model    = "SARA-N410-02B",
         .method   = SETTINGS_UPDATE_METHOD_COPS,
         .uact     = FEATURE_UNSUPPORTED,
-        .ubandsel = FEATURE_SUPPORTED,
+        .ubandsel = FEATURE_UNSUPPORTED,
         .mode     = MM_MODEM_MODE_4G,
         .bands_4g = { MM_MODEM_BAND_EUTRAN_1,  MM_MODEM_BAND_EUTRAN_2,  MM_MODEM_BAND_EUTRAN_3,
                       MM_MODEM_BAND_EUTRAN_4,  MM_MODEM_BAND_EUTRAN_5,  MM_MODEM_BAND_EUTRAN_8,