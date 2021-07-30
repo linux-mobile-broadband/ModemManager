999df00d58df4ea088e19ea9f9dd73a8bb077fb5
Aleksander Morgado <aleksander@aleksander.es>
2021-07-30 16:43:08 +0200

    fibocom: fix QMI device allowance
    E.g. for a FM150:
    
       [1627656003.230873] [cdc-wdm0] port contents loaded:
       [1627656003.231069] [cdc-wdm0]   bus: usb
       [1627656003.231179] [cdc-wdm0]   interface: /sys/devices/pci0000:00/0000:00:14.0/usb1/1-11/1-11.4/1-11.4.4/1-11.4.4:1.4
       [1627656003.231272] [cdc-wdm0]   device: /sys/devices/pci0000:00/0000:00:14.0/usb1/1-11/1-11.4/1-11.4.4
       [1627656003.231364] [cdc-wdm0]   driver: qmi_wwan
       [1627656003.231450] [cdc-wdm0]   vendor: 2cb7
       [1627656003.231536] [cdc-wdm0]   product: 0104
       [1627656003.231626] [cdc-wdm0]   revision: 0414
       [1627656003.231722] [base-manager] adding port cdc-wdm0 at sysfs path: /sys/devices/pci0000:00/0000:00:14.0/usb1/1-11/1-11.4/1-11.4.4/1-11.4.4:1.4/usbmisc/cdc-wdm0
       ...
       [1627656004.333874] [plugin/fibocom] port cdc-wdm0 filtered by implicit QMI driver

--- plugins/fibocom/mm-plugin-fibocom.c
+++ plugins/fibocom/mm-plugin-fibocom.c
@@ -113,6 +113,7 @@ mm_plugin_create (void)
                       MM_PLUGIN_ALLOWED_DRIVERS,    drivers,
                       MM_PLUGIN_ALLOWED_AT,         TRUE,
                       MM_PLUGIN_ALLOWED_MBIM,       TRUE,
+                      MM_PLUGIN_ALLOWED_QMI,        TRUE,
                       MM_PLUGIN_XMM_PROBE,          TRUE,
                       NULL));
 }
