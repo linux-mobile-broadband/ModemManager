8852cef8175bb5421cd14986ac3803985d5e6eae
Aleksander Morgado <aleksander@aleksander.es>
2021-10-13 22:04:53 +0200

    quectel: fix non-initialized GError
    Fixes https://gitlab.freedesktop.org/mobile-broadband/ModemManager/-/issues/442

--- plugins/quectel/mm-shared-quectel.c
+++ plugins/quectel/mm-shared-quectel.c
@@ -594,7 +594,7 @@ disable_location_gathering_parent_ready (MMIfaceModemLocation *self,
                                          GAsyncResult         *res,
                                          GTask                *task)
 {
-    GError  *error;
+    GError  *error = NULL;
     Private *priv;
 
     priv = get_private (MM_SHARED_QUECTEL (self));
