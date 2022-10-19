68123ac875ffeab9528977ca34e4db9a26a4140c
Aleksander Morgado <aleksandermj@chromium.org>
2022-10-19 11:09:30 +0000

    mmcli: update copyright years

--- cli/mmcli.c
+++ cli/mmcli.c
@@ -173,7 +173,7 @@ static void
 print_version_and_exit (void)
 {
     g_print (PROGRAM_NAME " " PROGRAM_VERSION "\n"
-             "Copyright (2011 - 2021) Aleksander Morgado\n"
+             "Copyright (2011 - 2022) Aleksander Morgado\n"
              "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl-2.0.html>\n"
              "This is free software: you are free to change and redistribute it.\n"
              "There is NO WARRANTY, to the extent permitted by law.\n"
