3aefc983b5d73b2c822807b23be9f76a95b11139
Aleksander Morgado <aleksandermj@chromium.org>
2023-03-30 12:38:05 +0000

    sms-part-3gpp|cdma: new fuzzer tests

---
+++ src/tests/test-sms-part-3gpp-fuzzer.c
@@ -0,0 +1,39 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2023 Google, Inc.
+ */
+
+#include <config.h>
+#include <string.h>
+#include <stdint.h>
+#include <glib.h>
+
+#define _LIBMM_INSIDE_MM
+#include <libmm-glib.h>
+
+#include "mm-sms-part-3gpp.h"
+
+int
+LLVMFuzzerTestOneInput (const uint8_t *data,
+                        size_t         size)
+{
+    g_autoptr(MMSmsPart) part = NULL;
+    g_autoptr(GError)    error = NULL;
+
+    if (!size)
+        return 0;
+
+    part = mm_sms_part_3gpp_new_from_binary_pdu (0, data, size, NULL, FALSE, &error);
+    g_assert (part || error);
+    return 0;
+}
