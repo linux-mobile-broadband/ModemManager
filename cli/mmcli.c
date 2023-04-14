19b910b60f13f9987b3f59cb062c322c6ecaea15
Aleksander Morgado <aleksandermj@chromium.org>
2023-04-14 08:48:35 +0000

    cli: update copyright

--- cli/mmcli.c
+++ cli/mmcli.c
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2011 Aleksander Morgado <aleksander@gnu.org>
+ * Copyright (C) 2011-2023 Aleksander Morgado <aleksander@aleksander.es>
  * Copyright (C) 2011 Google, Inc.
  */
 
@@ -173,7 +173,7 @@ static void
 print_version_and_exit (void)
 {
     g_print (PROGRAM_NAME " " PROGRAM_VERSION "\n"
-             "Copyright (2011 - 2022) Aleksander Morgado\n"
+             "Copyright (2011 - 2023) Aleksander Morgado\n"
              "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl-2.0.html>\n"
              "This is free software: you are free to change and redistribute it.\n"
              "There is NO WARRANTY, to the extent permitted by law.\n"
