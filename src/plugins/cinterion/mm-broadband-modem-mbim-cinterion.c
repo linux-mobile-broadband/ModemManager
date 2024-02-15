1befca4573885129dae8436b3a0a9c3002712d1a
Aleksander Morgado <aleksandermj@chromium.org>
2024-02-15 12:55:52 +0000

    cinterion: disable periodic signal checks in MBIM modems

--- src/plugins/cinterion/mm-broadband-modem-mbim-cinterion.c
+++ src/plugins/cinterion/mm-broadband-modem-mbim-cinterion.c
@@ -69,6 +69,7 @@ mm_broadband_modem_mbim_cinterion_new (const gchar *device,
                          MM_BASE_MODEM_DATA_NET_SUPPORTED, TRUE,
                          MM_BASE_MODEM_DATA_TTY_SUPPORTED, FALSE,
                          MM_IFACE_MODEM_SIM_HOT_SWAP_SUPPORTED, TRUE,
+                         MM_IFACE_MODEM_PERIODIC_SIGNAL_CHECK_DISABLED, TRUE,
                          MM_BROADBAND_MODEM_MBIM_INTEL_FIRMWARE_UPDATE_UNSUPPORTED, TRUE,
                          NULL);
 }
